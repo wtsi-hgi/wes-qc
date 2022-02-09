d1d08da14295b029afda8cb7c2e019219d1d8a19
RuthEberhardt <re3@sanger.ac.uk>
2022-02-09 15:04:56 +0000

    fixed dirs

--- misc/create_chr20_mt.py
+++ misc/create_chr20_mt.py
@@ -16,17 +16,16 @@ def main():
     with open(input_yaml, 'r') as y:
         inputs = yaml.load(y, Loader=yaml.FullLoader)
     mtdir = inputs['matrixtables_lustre_dir']
-    lustre_dir = inputs['hail_lustre_dir']
+    tmp_dir = "hdfs://spark-master:9820/"
     sc = pyspark.SparkContext()
-    hl.init(sc=sc, tmp_dir=lustre_dir, local_tmpdir=lustre_dir, default_reference="GRCh38")
+    hl.init(sc=sc, tmp_dir=tmp_dir, default_reference="GRCh38")
 
     mt_in_file = mtdir + "/gatk_unprocessed.mt"
     mt_out_file = mtdir + "/tests/gatk_unprocessed_chr20.mt"
     print("Reading input matrix")
     mt = hl.read_matrix_table(mt_in_file)
-    intervals = ['chr20']
     print("Subsetting to chr20")
-    mt = hl.filter_intervals(mt, [hl.parse_locus_interval('chr17')])
+    mt = hl.filter_intervals(mt, [hl.parse_locus_interval('chr20')])
     mt.write(mt_out_file, overwrite=True)
 
 if __name__ == '__main__':
