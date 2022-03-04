5f34556135ff51cd6fda2c817b2ea24754de2832
RuthEberhardt <re3@sanger.ac.uk>
2022-03-04 16:18:51 +0000

    next part of samle QC by pop

--- 2-sample_qc/4-find_population_outliers.py
+++ 2-sample_qc/4-find_population_outliers.py
@@ -2,6 +2,7 @@
 import hail as hl
 import pyspark
 from wes_qc.utils.utils import parse_config
+from gnomad.sample_qc.filtering import compute_stratified_metrics_filter
 
 
 def annotate_pops(pruned_mt_file: str, pop_ht_file: str, pops_annot_mt_file: str):
@@ -17,13 +18,47 @@ def annotate_pops(pruned_mt_file: str, pop_ht_file: str, pops_annot_mt_file: str
     mt.write(pops_annot_mt_file, overwrite=True)
 
 
+def stratified_sample_qc(pops_annot_mt_file: str, mt_qc_outfile: str, ht_qc_cols_outfile: str, qc_filter_file: str):
+    '''
+    Run sample QC and stratify by population
+    param str pops_annot_mt_file: population annotated MT file
+    param str mt_qc_outfile: sample QC MT file
+    param str ht_qccols_outfile: sample QC columns HT file
+    param str qc_filter_file: output file for stratified sample QC HT
+    '''
+    mt = hl.read_matrix_table(pops_annot_mt_file)
+    #run sample QC
+    mt_with_sampleqc = hl.sample_qc(mt, name='sample_qc')
+    #annotate with heterozygosity rate
+    mt_with_sampleqc = mt_with_sampleqc.annotate_cols(sample_qc=mt_with_sampleqc.sample_qc.annotate(
+        heterozygosity_rate=mt_with_sampleqc.sample_qc.n_het/mt_with_sampleqc.sample_qc.n_called))
+    mt_with_sampleqc.write(mt_qc_outfile, overwrite=True)
+    mt_with_sampleqc.cols().write(ht_qc_cols_outfile,  overwrite=True)
+    #stratify by pop
+    pop_ht = hl.read_table(ht_qc_cols_outfile)
+    qc_metrics = ['heterozygosity_rate', 'n_snp', 'r_ti_tv',
+                  'r_insertion_deletion', 'n_insertion', 'n_deletion', 'r_het_hom_var']
+
+    pop_filter_ht = compute_stratified_metrics_filter(
+        pop_ht, 
+        qc_metrics={
+            metric: pop_ht.sample_qc[metric] for metric in qc_metrics
+        },
+        strata={"qc_pop": pop_ht.assigned_pop},
+        )
+    
+    pop_ht = pop_ht.annotate_globals(**pop_filter_ht.index_globals())
+    pop_ht = pop_ht.annotate(**pop_filter_ht[pop_ht.key]).persist()
+    checkpoint = pop_ht.aggregate(hl.agg.count_where(
+        hl.len(pop_ht.qc_metrics_filters) == 0))
+    print(f'{checkpoint} exome samples found passing pop filtering')
+    pop_ht.write(qc_filter_file, overwrite=True)
+
 
 def main():
     #set up
     inputs = parse_config()
-    importmtdir = inputs['load_matrixtables_lustre_dir']
     mtdir = inputs['matrixtables_lustre_dir']
-    annotdir = inputs['annotation_lustre_dir']
 
     #initialise hail
     tmp_dir = "hdfs://spark-master:9820/"
@@ -37,6 +72,12 @@ def main():
     pops_annot_mt_file = mtdir + "mt_ldpruned_pops_annotated.mt"
     annotate_pops(pruned_mt_file, pop_ht_file, pops_annot_mt_file)
 
+    #run sample QC and stratify by population
+    mt_qc_outfile = mtdir + "mt_pops_sampleqc.mt"
+    ht_qc_cols_outfile = mtdir + "mt_pops_sampleqc.ht"
+    qc_filter_file = mtdir + "mt_pops_QC_filters.mt"
+    stratified_sample_qc(pops_annot_mt_file, mt_qc_outfile, ht_qc_cols_outfile, qc_filter_file)
+
 
 if __name__ == '__main__':
-    main() 
+    main()
