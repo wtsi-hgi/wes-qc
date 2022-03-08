9b543f86998b3a134ba83bff59ad890cb0518523
RuthEberhardt <re3@sanger.ac.uk>
2022-03-08 09:18:13 +0000

    non-stratified sample QC

--- 2-sample_qc/5-sample_qc_non_stratified.py
+++ 2-sample_qc/5-sample_qc_non_stratified.py
@@ -3,12 +3,13 @@ import hail as hl
 import pyspark
 from wes_qc.utils.utils import parse_config
 
-def run_sample_qc(raw_mt_file, pops_qc_filter_file, mt_sample_qc_file):
+def run_sample_qc(raw_mt_file, pops_qc_filter_file, mt_sample_qc_file, ht_sample_qc_file):
     '''
     Run sample QC on raw variants
     :param str raw_mt_file: raw mt file 
     :param str pops_qc_filter_file: output from stratified QC
     :param str mt_sample_qc_file: output MT
+    :param str ht_sample_qc_file: output HT, cols only
     '''
     mt = hl.read_matrix_table(raw_mt_file)
     mt = hl.sample_qc(mt)
@@ -22,6 +23,8 @@ def run_sample_qc(raw_mt_file, pops_qc_filter_file, mt_sample_qc_file):
     mt = mt.annotate_cols(filter_fail_count=stratified_ht[mt.s].num_fails)
     print("Writing annotated MT to file")
     mt.write(mt_sample_qc_file, overwrite=True)
+    # also write just the cols as an HT
+    mt.cols().write(ht_sample_qc_file,  overwrite=True)
 
 
 def main():
@@ -39,7 +42,8 @@ def main():
     raw_mt_file = mtdir + "gatk_unprocessed.mt"
     mt_sample_qc_file = mtdir + "non_stratified_sample_qc.mt"
     pops_qc_filter_file = mtdir + "mt_pops_QC_filters.ht"
-    run_sample_qc(raw_mt_file, pops_qc_filter_file, mt_sample_qc_file)
+    ht_sample_qc_file = mtdir + "non_stratified_sample_qc_cols.ht"
+    run_sample_qc(raw_mt_file, pops_qc_filter_file, mt_sample_qc_file, ht_sample_qc_file)
 
 
 if __name__ == '__main__':
