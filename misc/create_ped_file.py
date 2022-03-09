92f013f8974f9a4303e4a92264ae6862c2108116
RuthEberhardt <re3@sanger.ac.uk>
2022-03-09 10:44:39 +0000

    create ped file

---
+++ misc/create_ped_file.py
@@ -0,0 +1,34 @@
+# Create ped file for trios to use in variant QC
+from wes_qc.utils.utils import parse_config
+
+
+def parse_manifest(manifest_file):
+    '''
+    Parse manifest file and return a dict keyed by proband.
+    We only want complete trios.
+    '''
+    trios = {}
+    with open(manifest_file, 'r') as f:
+        lines = f.readlines()
+        for l in lines:
+            linedata = l.split("\t")
+            print(linedata)
+            exit(0)
+
+    return trios
+
+
+def write_ped():
+    pass
+
+
+def main():
+    inputs = parse_config()
+    resourcedir = inputs['resource_dir']
+    manifest_file = resourcedir + "all_samples_with_proceed_and_seq_info.txt"
+
+    triodata = parse_manifest(manifest_file)
+    write_ped()
+
+if __name__ == '__main__':
+    main()
