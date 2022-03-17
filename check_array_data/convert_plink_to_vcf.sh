5e3e9852d936cd3a3ea035cbd0aa2b5a2320d9da
RuthEberhardt <re3@sanger.ac.uk>
2022-03-17 14:16:53 +0000

    convert plink to vcf

--- check_array_data/convert_plink_to_vcf.sh
+++ check_array_data/convert_plink_to_vcf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 plinkdata=/lustre/scratch119/humgen/projects/birth_cohort_wes/alspac/existing_data/ARRAY_2020-09-03/data
-outvcf=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/plink_vcfg
+outvcf=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/plink_vcf
 plink=/software/hgi/installs/plink/dev-05-03-2022/plink2
 
 ${plink} --pfile ${plinkdata} --recode vcf --out ${vcf}
