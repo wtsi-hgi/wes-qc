140486e518f329bccc94a71570930765a707c717
RuthEberhardt <re3@sanger.ac.uk>
2022-03-17 14:57:50 +0000

    change pfile to bfile

--- check_array_data/convert_plink_to_vcf.sh
+++ check_array_data/convert_plink_to_vcf.sh
@@ -2,7 +2,7 @@
 
 plinkdata=/lustre/scratch119/humgen/projects/birth_cohort_wes/alspac/existing_data/ARRAY_2020-09-03/data
 outvcf=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/plink_vcf
-#plink=/software/hgi/installs/plink/dev-05-03-2022/plink2
-plink=/software/hgi/installs/plink/1.90-beta-05-03-2022/plink
+plink=/software/hgi/installs/plink/dev-05-03-2022/plink2
+#plink=/software/hgi/installs/plink/1.90-beta-05-03-2022/plink
 
-${plink} --pfile ${plinkdata} --recode vcf --out ${outvcf}
+${plink} --bfile ${plinkdata} --recode vcf --out ${outvcf}
