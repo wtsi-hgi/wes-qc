ecf35036f01ba7c8c7bfad830bb141b322b687a9
RuthEberhardt <re3@sanger.ac.uk>
2022-03-17 15:33:54 +0000

    tidy up

--- check_array_data/convert_plink_to_vcf.sh
+++ check_array_data/convert_plink_to_vcf.sh
@@ -3,6 +3,9 @@
 plinkdata=/lustre/scratch119/humgen/projects/birth_cohort_wes/alspac/existing_data/ARRAY_2020-09-03/data
 outvcf=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/plink_vcf
 plink=/software/hgi/installs/plink/dev-05-03-2022/plink2
-#plink=/software/hgi/installs/plink/1.90-beta-05-03-2022/plink
 
 ${plink} --bfile ${plinkdata} --recode vcf --out ${outvcf}
+
+bgzip -c ${outvcf}.vcf > ${outvcf}.vcf.gz
+
+tabix -p vcf ${outvcf}.vcf.gz
