8ac7048c53460c77159c84f4a7005788b77717aa
RuthEberhardt <re3@sanger.ac.uk>
2022-03-18 11:16:46 +0000

    different ref

--- check_array_data/lift_array_vcf_to_b38.sh
+++ check_array_data/lift_array_vcf_to_b38.sh
@@ -6,7 +6,8 @@ arrayvcf=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotyp
 liftoverscript=/nfs/users/nfs_p/pd3/cvs/vr-codebase/scripts/lift-over
 # resources
 b37fa=/lustre/scratch119/humgen/projects/ddd/resources/v1.2/hs37d5.fasta
-b38fa=/lustre/scratch118/humgen/resources/ref/Homo_sapiens/HS38DH/hs38DH.fa
+#b38fa=/lustre/scratch118/humgen/resources/ref/Homo_sapiens/HS38DH/hs38DH.fa
+b38fa=/lustre/scratch118/humgen/resources/ref/Homo_sapiens/GRCh38_15/Homo_sapiens.GRCh38_15.fa
 chain=/lustre/scratch118/humgen/resources/liftover/hg19ToHg38.over.chain
 # output
 prefix=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/liftover
