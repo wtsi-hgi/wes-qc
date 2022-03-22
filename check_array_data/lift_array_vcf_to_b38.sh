4f3fdf8ec577336979f43d59457d566f6cfee4f6
RuthEberhardt <re3@sanger.ac.uk>
2022-03-22 15:14:39 +0000

    new liftover script version

--- check_array_data/lift_array_vcf_to_b38.sh
+++ check_array_data/lift_array_vcf_to_b38.sh
@@ -5,7 +5,7 @@ arrayvcf=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotyp
 # wrapper script for lift over
 liftoverscript=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/tools/lift-over
 # resources
-b37fa=/lustre/scratch118/humgen/resources/ref/Homo_sapiens/1000Genomes_hs37d5/hs37d5.fasta
+b37fa=/lustre/scratch118/humgen/resources/ref/Homo_sapiens/1000Genomes_hs37d5/hs37d5.fa
 #b38fa=/lustre/scratch118/humgen/resources/ref/Homo_sapiens/HS38DH/hs38DH.fa
 b38fa=/lustre/scratch118/humgen/resources/ref/Homo_sapiens/GRCh38_15/Homo_sapiens.GRCh38_15.fa
 chain=/lustre/scratch118/humgen/resources/liftover/hg19ToHg38.over.chain
