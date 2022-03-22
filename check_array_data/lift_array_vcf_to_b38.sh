3bc809ea2285fa39c7f463accd9f1b0a737f251f
RuthEberhardt <re3@sanger.ac.uk>
2022-03-22 14:29:49 +0000

    new liftover script version

--- check_array_data/lift_array_vcf_to_b38.sh
+++ check_array_data/lift_array_vcf_to_b38.sh
@@ -3,15 +3,18 @@
 # infile
 arrayvcf=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/plink_vcf.vcf.gz
 # wrapper script for lift over
-liftoverscript=/nfs/users/nfs_p/pd3/cvs/vr-codebase/scripts/lift-over
+liftoverscript=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/tools/lift-over
 # resources
-b37fa=/lustre/scratch119/humgen/projects/ddd/resources/v1.2/hs37d5.fasta
+b37fa=/lustre/scratch118/humgen/resources/ref/Homo_sapiens/1000Genomes_hs37d5/hs37d5.fasta
 #b38fa=/lustre/scratch118/humgen/resources/ref/Homo_sapiens/HS38DH/hs38DH.fa
 b38fa=/lustre/scratch118/humgen/resources/ref/Homo_sapiens/GRCh38_15/Homo_sapiens.GRCh38_15.fa
 chain=/lustre/scratch118/humgen/resources/liftover/hg19ToHg38.over.chain
+#chain=/lustre/scratch118/humgen/resources/liftover/grch37tohg38.chain
 # output
 prefix=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/liftover
 outfile=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/plink_vcf_b38_liftover.vcf.gz
+
+export PERL5LIB=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/tools/:$PERL5LIB
 
 ${liftoverscript} -i ${arrayvcf} -p ${prefix} -o ${b37fa} -n ${b38fa} -c ${chain} | bcftools sort | bgzip -c > ${outfile}
 
