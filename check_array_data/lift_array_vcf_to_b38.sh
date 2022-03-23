ea7edaa8b384896fb07c98100f96de38b67bf1c1
RuthEberhardt <re3@sanger.ac.uk>
2022-03-23 14:09:28 +0000

    split sort and tabix off

--- check_array_data/lift_array_vcf_to_b38.sh
+++ check_array_data/lift_array_vcf_to_b38.sh
@@ -13,17 +13,20 @@ chain=/lustre/scratch118/humgen/resources/liftover/hg19ToHg38.over.chain
 prefix=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/liftover
 outfile=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/plink_vcf_b38_liftover.vcf.gz
 
-outfile_tmp = ${outfile}_tmp
+outfile_l=${outfile}_lifted
+outfile_header_fix=${outfile}_hf
 
 #liftOver path
 liftoverpath=/software/hgi/installs/liftOver
 
 export PERL5LIB=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/tools/:$PERL5LIB
 
-${liftoverscript} -i ${arrayvcf} -p ${prefix} -o ${b37fa} -n ${b38fa} -c ${chain} -l ${liftoverpath} > ${outfile_tmp}
+${liftoverscript} -i ${arrayvcf} -p ${prefix} -o ${b37fa} -n ${b38fa} -c ${chain} -l ${liftoverpath} > ${outfile_l}
 
-sed s'/contig=<ID=/contig=<ID=chr/g' ${oufile_tmp} | bcftools sort -Oz -o ${outfile}
+sed s'/contig=<ID=/contig=<ID=chr/g' ${oufile_tmp} > ${outfile_header_fix}
+bcftools sort -Oz -o ${outfile} ${outfile_header_fix}
 
 tabix -p vcf ${outfile}
 
-#rm ${outfile_tmp}
+#rm ${outfile_l}
+#rm ${outfile_header_fix}
