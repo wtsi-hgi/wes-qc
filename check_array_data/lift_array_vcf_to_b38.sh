9e0d20bdfa3dd46c8b96f67ae40a6c16486d4583
RuthEberhardt <re3@sanger.ac.uk>
2022-03-23 08:19:04 +0000

    tidy

--- check_array_data/lift_array_vcf_to_b38.sh
+++ check_array_data/lift_array_vcf_to_b38.sh
@@ -6,10 +6,8 @@ arrayvcf=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotyp
 liftoverscript=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/tools/lift-over
 # resources
 b37fa=/lustre/scratch118/humgen/resources/ref/Homo_sapiens/1000Genomes_hs37d5/hs37d5.fa
-#b38fa=/lustre/scratch118/humgen/resources/ref/Homo_sapiens/HS38DH/hs38DH.fa
 b38fa=/lustre/scratch118/humgen/resources/ref/Homo_sapiens/GRCh38_15/Homo_sapiens.GRCh38_15.fa
 chain=/lustre/scratch118/humgen/resources/liftover/hg19ToHg38.over.chain
-#chain=/lustre/scratch118/humgen/resources/liftover/grch37tohg38.chain
 # output
 prefix=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/liftover
 outfile=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/plink_vcf_b38_liftover.vcf.gz
