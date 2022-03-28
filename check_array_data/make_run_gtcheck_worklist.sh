b855b4bb077b4ef7bb117f6a555f3d7a37b4ec9c
RuthEberhardt <re3@sanger.ac.uk>
2022-03-28 14:47:20 +0100

    run gtcheck job array

--- check_array_data/make_run_gtcheck_worklist.sh
+++ check_array_data/make_run_gtcheck_worklist.sh
@@ -14,7 +14,7 @@ worklist="/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genoty
 for f in `cat ${vcflist}`
     do
     vcf="/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/gatk_vcfs/"${f}
-    pos=${vcf::-7}
+    pos=${vcf::-12}
     cmd=${bcftools}" gtcheck -S gt:"${samplesg}" -S qry:"${samplesv}"  --n-matches 100 -g "${plinkvcf}" "${vcf}" > "${outfile}${pos}
     echo ${cmd}
     done > ${worklist}
