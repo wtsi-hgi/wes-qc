71f3694e7d5528c8f649d2870d9c3e47deb34cd6
RuthEberhardt <re3@sanger.ac.uk>
2022-03-28 14:05:47 +0100

    run gtcheck as job array

---
+++ check_array_data/run_gtcheck_job_array.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ 
+vcf=$(sed -n -e ${LSB_JOBINDEX}p /lustre/scratch123/hgi/projects/birth_cohort_wes/qc/gatk_vcf_list.txt)
+
+bcftools="/nfs/users/nfs_p/pd3/bcftools/dist/bin/bcftools"
+samplesg="/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/plink_samples.txt"
+samplesv="/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/gatk_samples.txt"
+plinkvcf="/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/plink_vcf_b38_liftover.bcf"
+outdir="/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/run_gtcheck/"
+
+outfile=${outdir}"gtcheck_out_"${LSB_JOBINDEX}
+
+cmd=${bcftools}" gtcheck -S gt:"${samplesg}" -S qry:"${samplesv}"  --n-matches 100 -g "${plinkvcf}" "${vcf}" > "${outfile}
+echo ${cmd}
+
+${cmd}
