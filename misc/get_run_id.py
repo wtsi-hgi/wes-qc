0c9f652f4fb92ccab60ab066c20e9a04ca9b07dd
RuthEberhardt <re3@sanger.ac.uk>
2022-04-04 09:09:50 +0100

    get run id info

--- misc/get_run_id.py
+++ misc/get_run_id.py
@@ -3,6 +3,7 @@
 
 sequencing_info_file = '/lustre/scratch119/humgen/projects/birth_cohort_wes/genestack/6407_QC_pass_seq_Batches.csv'
 idmap_file = '/lustre/scratch119/humgen/projects/birth_cohort_wes/genestack/sequencescape_dump_study_6407.txt'
+outfile = '/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/resources/sequencing_batches.txt'
 
 sanger_ega = {}
 ega_runid = {}
@@ -19,7 +20,13 @@ with open(sequencing_info_file, 'r') as f2:
     for l2 in lines2:
         if not l2.startswith('sanger_sample_id'):
             l2data = l2.split(",")
-            print(l2data)
-            exit(0)
             ega = sanger_ega[l2data[0]]
             runid = l2data[3]
+            ega_runid[ega] = runid
+
+with open(outfile, 'w') as o:
+    o.write(("\t").join(['ega', 'runid']))
+    o.write("\n")
+    for e in ega_runid.keys():
+        o.write(("\t").join([e, ega_runid[e]]))
+        o.write("\n")
