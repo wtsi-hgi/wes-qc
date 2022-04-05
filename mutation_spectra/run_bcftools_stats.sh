2aa548a1cc00b7fa4529754a14169743107bdf7f
RuthEberhardt <re3@sanger.ac.uk>
2022-04-05 15:31:52 +0100

    run bcftools stats

--- mutation_spectra/run_bcftools_stats.sh
+++ mutation_spectra/run_bcftools_stats.sh
@@ -6,9 +6,13 @@ chr=$2
 bcftools=/nfs/users/nfs_p/pd3/bcftools/dist/bin/bcftools
 bcfdir=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/gatk_vcfs/per_chromosome/
 outdir=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/mutation_spectra/
+tmpdir=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/mutation_spectra/tmp/
+tmpvcf=tmpdir${sample}_chr${chr}.bcf
 
 echo "Running with sample:"$1" chromosome:chr"$2
 
-cmd='"'${bcftools}" view -s "${sample}" --trim-alt-alleles -Ou "${bcfdir}"chr"${chr}".bcf | "${bcftools}" stats  -i 'N_ALT>1' > "${outdir}${sample}"_chr"${chr}".stats"'"'
-echo ${cmd}
-bash -c ${cmd}
+${bcftools} view -s ${sample} --trim-alt-alleles -Ob -o ${tmpbcf} ${bcfdir}chr${chr}.bcf 
+${bcftools} index ${tmpvcf}
+
+${bcftools} stats  -i 'N_ALT>1' ${tmpbcf} > ${outdir}${sample}_chr${chr}.stats
+
