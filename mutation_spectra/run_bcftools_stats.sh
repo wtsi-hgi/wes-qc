1d3354a34daa140541684df789b0b1a26c824690
RuthEberhardt <re3@sanger.ac.uk>
2022-04-05 16:04:14 +0100

    run bcftools stats

--- mutation_spectra/run_bcftools_stats.sh
+++ mutation_spectra/run_bcftools_stats.sh
@@ -7,12 +7,9 @@ bcftools=/nfs/users/nfs_p/pd3/bcftools/dist/bin/bcftools
 bcfdir=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/gatk_vcfs/per_chromosome/
 outdir=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/mutation_spectra/
 tmpdir=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/mutation_spectra/tmp/
-tmpbcf=${tmpdir}${sample}_chr${chr}.bcf
 
 echo "Running with sample:"$1" chromosome:chr"$2
 
-${bcftools} view -s ${sample} --trim-alt-alleles -Ob -o ${tmpbcf} ${bcfdir}chr${chr}.bcf 
-${bcftools} index ${tmpvcf}
+$cmd = ${bcftools}" view -s "${sample}" --trim-alt-alleles -Ou | "${bcftools}" stats  -i 'N_ALT>1' "${tmpbcf}" > "${outdir}${sample}"_chr"${chr}".stats"
 
-${bcftools} stats  -i 'N_ALT>1' ${tmpbcf} > ${outdir}${sample}_chr${chr}.stats
-
+eval "${cmd}"
