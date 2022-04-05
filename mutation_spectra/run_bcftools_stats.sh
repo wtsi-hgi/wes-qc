9b460aa3e177861f49080c5a29407fbfa6c2b5d8
RuthEberhardt <re3@sanger.ac.uk>
2022-04-05 14:58:54 +0100

    run bcftools stats

--- mutation_spectra/run_bcftools_stats.sh
+++ mutation_spectra/run_bcftools_stats.sh
@@ -7,4 +7,6 @@ bcftools=/nfs/users/nfs_p/pd3/bcftools/dist/bin/bcftools
 bcfdir=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/gatk_vcfs/per_chromosome/
 outdir=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/mutation_spectra/
 
+echo "Running with sample:"$1" chromosome:chr"$2
+
 ${bcftools} view -s ${sample}  --trim-alt-alleles -Ou ${bcfdir}chr${chr}.bcf | ${bcftools} stats  -i 'N_ALT>1' > ${outdir}${sample}_chr${chr}.stats
