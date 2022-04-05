d0b087ba0d0aac1d9e2a5ad3e8d134b69a4277d2
RuthEberhardt <re3@sanger.ac.uk>
2022-04-05 15:06:55 +0100

    run bcftools stats

--- mutation_spectra/bsub_bcftools_stats_jobarray.sh
+++ mutation_spectra/bsub_bcftools_stats_jobarray.sh
@@ -7,10 +7,15 @@ wdir=$(pwd)
 script=${wdir}/run_bcftools_stats.sh
 echo ${script}
 
-sample=$(sed -n -e ${LSB_JOBINDEX}p ${sample_list})
+sampleraw=$(sed -n -e ${LSB_JOBINDEX}p ${sample_list})
+echo ${sampleraw}
+#strip new line
+sample="$(echo "$sampleraw"|tr -d '\n')"
+echo ${sample}
 
 for chr in {{1..22},X,Y}
 do
+echo ${chr}
 script ${sample} ${chr}
 done
 
