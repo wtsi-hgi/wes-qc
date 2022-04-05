cc46ac88e5611e339b612f44303b9ba1e60d4a89
RuthEberhardt <re3@sanger.ac.uk>
2022-04-05 16:23:56 +0100

    run bcftools stats

--- mutation_spectra/run_bcftools_stats.sh
+++ mutation_spectra/run_bcftools_stats.sh
@@ -8,8 +8,13 @@ bcfdir=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/gatk_vcfs/per_chromos
 outdir=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/mutation_spectra/
 tmpdir=/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/mutation_spectra/tmp/
 
+outfile=${outdir}${sample}_${chr}.stats
+bcf=${bcfdir}chr${chr}.bcf
+
 echo "Running with sample:"$1" chromosome:chr"$2
 
-$cmd=${bcftools}" view -s "${sample}" --trim-alt-alleles -Ou "${bcfdir}"chr"${chr}".bcf | "${bcftools}" stats  -i 'N_ALT>1' "${tmpbcf}" > "${outdir}${sample}"_chr"${chr}".stats"
+#$cmd=${bcftools}" view -s "${sample}" --trim-alt-alleles -Ou "${bcfdir}"chr"${chr}".bcf | "${bcftools}" stats  -i 'N_ALT>1' "${tmpbcf}" > "${outdir}${sample}"_chr"${chr}".stats"
 
-eval "${cmd}"
+#eval "${cmd}"
+
+/nfs/users/nfs_p/pd3/bcftools/dist/bin/bcftools view -s ${sample} --trim-alt-alleles -Ou ${bcf} | /nfs/users/nfs_p/pd3/bcftools/dist/bin/bcftools stats  -i 'N_ALT>1' > ${outfile}
