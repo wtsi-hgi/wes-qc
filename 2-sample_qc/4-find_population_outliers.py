405b62047d8a0c58ba983e62e4dc8c556fb9746e
RuthEberhardt <re3@sanger.ac.uk>
2022-04-19 15:50:41 +0100

    stratify sample qc by sequencing location and pop

--- 2-sample_qc/4-find_population_outliers.py
+++ 2-sample_qc/4-find_population_outliers.py
@@ -9,7 +9,7 @@ def annotate_mt(raw_mt_file: str, pop_ht_file: str, runid_file: str, annotated_m
     '''
     Annotate mt with superpopulation and sequencing runid
     :param str raw_mt_file: raw mt file
-    :param str pop_ht_file: file with popuklation annotations
+    :param str pop_ht_file: file with population annotations
     :param str runid_file: metadata file to annotate run ids
     :param str annotated_mt_file: annotated mt file
     '''
@@ -21,7 +21,7 @@ def annotate_mt(raw_mt_file: str, pop_ht_file: str, runid_file: str, annotated_m
                 .when(mt.s.startswith('Z'), 'Bristol')
                 .default("")
                 )
-    mt = mt.annotate_cols(sequencing_location = seq_expr).key_cols_by('s')
+    mt = mt.annotate_cols(sequencing_location=seq_expr).key_cols_by('s')
     pop_ht = hl.read_table(pop_ht_file)
     mt = mt.annotate_cols(assigned_pop=pop_ht[mt.s].pop)
     mt.write(annotated_mt_file, overwrite=True)
@@ -37,43 +37,46 @@ def stratified_sample_qc(annotated_mt_file: str, mt_qc_outfile: str, ht_qc_cols_
     aram str annotdir: output directory for annotations
     '''
     mt = hl.read_matrix_table(annotated_mt_file)
-    #filter to autosomes only
+    # filter to autosomes only
     mt = mt.filter_rows(mt.locus.in_autosome())
 
-    #filter MT by depth/gq/vaf
+    # filter MT by depth/gq/vaf
     min_dp = 20
     min_gq = 20
     min_vaf = 0.25
     if min_dp > 0 or min_gq > 0 or min_vaf > 0:
         vaf = mt.AD[1] / hl.sum(mt.AD)
-        print("Filtering input MT by depth: DP=" + str(min_dp) + ", genotype quality: GQ=" + str(min_gq) + ", VAF: VAF=" + str(min_vaf))
-        filter_condition = ( (mt.GT.is_het() & (vaf > min_vaf) & (mt.DP > min_dp) & (mt.GQ > min_gq)) | 
-                   (mt.GT.is_hom_ref() & (mt.DP > min_dp) & (mt.GQ > min_gq)) |
-                   (mt.GT.is_hom_var() & (mt.DP > min_dp) & (mt.GQ > min_gq)))
-        fraction_filtered = mt.aggregate_entries(hl.agg.fraction(~filter_condition))
-        print(f'Filtering {fraction_filtered * 100:.2f}% entries out of downstream analysis.')
+        print("Filtering input MT by depth: DP=" + str(min_dp) +
+              ", genotype quality: GQ=" + str(min_gq) + ", VAF: VAF=" + str(min_vaf))
+        filter_condition = ((mt.GT.is_het() & (vaf > min_vaf) & (mt.DP > min_dp) & (mt.GQ > min_gq)) |
+                            (mt.GT.is_hom_ref() & (mt.DP > min_dp) & (mt.GQ > min_gq)) |
+                            (mt.GT.is_hom_var() & (mt.DP > min_dp) & (mt.GQ > min_gq)))
+        fraction_filtered = mt.aggregate_entries(
+            hl.agg.fraction(~filter_condition))
+        print(
+            f'Filtering {fraction_filtered * 100:.2f}% entries out of downstream analysis.')
         mt = mt.filter_entries(filter_condition)
 
-    #run sample QC
+    # run sample QC
     mt_with_sampleqc = hl.sample_qc(mt, name='sample_qc')
-    #annotate with heterozygosity rate
+    # annotate with heterozygosity rate
     mt_with_sampleqc = mt_with_sampleqc.annotate_cols(sample_qc=mt_with_sampleqc.sample_qc.annotate(
         heterozygosity_rate=mt_with_sampleqc.sample_qc.n_het/mt_with_sampleqc.sample_qc.n_called))
     mt_with_sampleqc.write(mt_qc_outfile, overwrite=True)
     mt_with_sampleqc.cols().write(ht_qc_cols_outfile,  overwrite=True)
-    #stratify by pop
+    # stratify by pop
     pop_ht = hl.read_table(ht_qc_cols_outfile)
     qc_metrics = ['heterozygosity_rate', 'n_snp', 'r_ti_tv', 'n_transition', 'n_transversion',
                   'r_insertion_deletion', 'n_insertion', 'n_deletion', 'r_het_hom_var']
 
     pop_filter_ht = compute_stratified_metrics_filter(
-        pop_ht, 
+        pop_ht,
         qc_metrics={
             metric: pop_ht.sample_qc[metric] for metric in qc_metrics
         },
         strata={"qc_pop": pop_ht.assigned_pop},
-        )
-    
+    )
+
     pop_ht = pop_ht.annotate_globals(**pop_filter_ht.index_globals())
     pop_ht = pop_ht.annotate(**pop_filter_ht[pop_ht.key]).persist()
     checkpoint = pop_ht.aggregate(hl.agg.count_where(
@@ -86,30 +89,31 @@ def stratified_sample_qc(annotated_mt_file: str, mt_qc_outfile: str, ht_qc_cols_
 
 
 def main():
-    #set up
+    # set up
     inputs = parse_config()
     mtdir = inputs['matrixtables_lustre_dir']
     annotdir = inputs['annotation_lustre_dir']
     resourcesdir = inputs['resource_dir']
 
-    #initialise hail
+    # initialise hail
     tmp_dir = "hdfs://spark-master:9820/"
     sc = pyspark.SparkContext()
     hadoop_config = sc._jsc.hadoopConfiguration()
     hl.init(sc=sc, tmp_dir=tmp_dir, default_reference="GRCh38")
 
-    #annotate mt with runid and pop
+    # annotate mt with runid and pop
     raw_mt_file = mtdir + "gatk_unprocessed.mt"
     pop_ht_file = mtdir + "pop_assignments.ht"
     runid_file = resourcesdir + "sequencing_batches.txt"
     annotated_mt_file = mtdir + "gatk_unprocessed_with_pop_and_runid.mt"
     annotate_mt(raw_mt_file, pop_ht_file, runid_file, annotated_mt_file)
 
-    #run sample QC and stratify by population
+    # run sample QC and stratify by population
     mt_qc_outfile = mtdir + "mt_pops_sampleqc.mt"
     ht_qc_cols_outfile = mtdir + "mt_pops_sampleqc.ht"
     qc_filter_file = mtdir + "mt_pops_QC_filters.ht"
-    stratified_sample_qc(annotated_mt_file, mt_qc_outfile, ht_qc_cols_outfile, qc_filter_file, annotdir)
+    stratified_sample_qc(annotated_mt_file, mt_qc_outfile,
+                         ht_qc_cols_outfile, qc_filter_file, annotdir)
 
 
 if __name__ == '__main__':
