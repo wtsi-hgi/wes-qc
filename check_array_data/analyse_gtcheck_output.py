c00af55e3228281ad3385bcceda46610c1349de8
RuthEberhardt <re3@sanger.ac.uk>
2022-04-22 13:39:47 +0100

    analyse gtcheck output

--- check_array_data/analyse_gtcheck_output.py
+++ check_array_data/analyse_gtcheck_output.py
@@ -5,12 +5,12 @@
 # and is that sample in the gneotyping file?
 
 
-def find_plink_samples(plinksamples_file):
+def get_sample_list(samples_file):
     '''
     create a list of samples that are in the plink data and parse to remove repetition (samples in plink are in format 123A_123A)
     '''
     samples = []
-    with open(plinksamples_file, 'r') as f:
+    with open(samples_file, 'r') as f:
         samples = f.readlines()
         samples = [x.strip() for x in samples]
         samples = [x.split("_")[0] for x in samples]
@@ -18,7 +18,7 @@ def find_plink_samples(plinksamples_file):
     return samples
 
 
-def parse_metadata(metadata_file, gtcheck_duplicates_file):
+def parse_metadata(metadata_file, wes_samples, gtcheck_duplicates_file):
     '''
     create a dict of WES sample id and genotyping sample id
     identify genotyping sample ids that correspond to more than one WES sample and print to file
@@ -36,6 +36,8 @@ def parse_metadata(metadata_file, gtcheck_duplicates_file):
                 wes_sample = ldata[0]
             elif ldata[25].startswith('EGA'):
                 wes_sample = ldata[25]
+            if not wes_sample in wes_samples:#we only want to include samples in the WES data
+                continue
             plink_sample = ldata[11]
             sample_map[wes_sample] = plink_sample
 
@@ -80,23 +82,23 @@ def parse_gtcheck_output(gtcheck_output_file, plink_samples, sample_map, gtcheck
             # score of < 0.05 seen as good so check if the samples match - if they don't it is a mismatch
             if score < 0.05:
                 if not sample_map[wes_sample] == plink_sample:
-                    mismatches.append(l)
+                    mismatches.append(l.rstrip() + "\t" + sample_map[wes_sample] + "\n")
             # score >= 0.05 is not a good match - but check the top hit anyway just in case
             else:
                 if sample_map[wes_sample] == plink_sample:
                     if plink_sample in plink_samples:
-                        dodgy_samples[wes_sample] = {'line': l.rstrip(), 'status': 'match', 'in_plink': 'yes'}
+                        dodgy_samples[wes_sample] = {'line': l.rstrip(), 'status': 'match', 'in_plink': 'yes', 'expected_match':sample_map[wes_sample]}
                     else:
-                        dodgy_samples[wes_sample] = {'line': l.rstrip(), 'status': 'match', 'in_plink': 'no'}
+                        dodgy_samples[wes_sample] = {'line': l.rstrip(), 'status': 'match', 'in_plink': 'no', 'expected_match':sample_map[wes_sample]}
                 else:
                     if plink_sample in plink_samples:
-                        dodgy_samples[wes_sample] = {'line': l.rstrip(), 'status': 'mismatch', 'in_plink': 'yes'}
+                        dodgy_samples[wes_sample] = {'line': l.rstrip(), 'status': 'mismatch', 'in_plink': 'yes', 'expected_match':sample_map[wes_sample]}
                     else:
-                        dodgy_samples[wes_sample] = {'line': l.rstrip(), 'status': 'mismatch', 'in_plink': 'no'}
+                        dodgy_samples[wes_sample] = {'line': l.rstrip(), 'status': 'mismatch', 'in_plink': 'no', 'expected_match':sample_map[wes_sample]}
 
     with open(gtcheck_mismatches_file, 'w') as o1:
         o1.write("#samples with mismatches between WES ID and genotyping ID\n")
-        o1.write(("\t").join(['#EGA', 'fam_id_top_hit', 'sum_discordance', 'nsites', "discordance/nsites"]))
+        o1.write(("\t").join(['#EGA', 'fam_id_top_hit', 'sum_discordance', 'nsites', 'discordance/nsites', 'expected_match']))
         o1.write("\n")
         for m in mismatches:
             o1.write(m)
@@ -104,24 +106,26 @@ def parse_gtcheck_output(gtcheck_output_file, plink_samples, sample_map, gtcheck
     with open(gtcheck_dodgy_samples_file, 'w') as o2:
         o2.write("#samples with high discordance score\n")
         o2.write(("\t").join(['#EGA', 'fam_id_top_hit', 'sum_discordance',
-                 'nsites', "discordance/nsites", "status", "id_in_plink_file"]))
+                 'nsites', "discordance/nsites", "status", "id_in_plink_file", "expected_match"]))
         o2.write("\n")
         for s in dodgy_samples.keys():
             o2.write(dodgy_samples[s]['line'] + "\t" + dodgy_samples[s]
-                     ['status'] + "\t" + dodgy_samples[s]['in_plink'] + "\n")
+                     ['status'] + "\t" + dodgy_samples[s]['in_plink'] + "\t" + dodgy_samples[s]['expected_match'] + "\n")
 
 
 def main():
     gtcheck_output_file = '/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/whole_exome_output/gtcheck_best_hits.txt'
-    plinksamples_file = '/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/plink_samples.txt'
+    plink_samples_file = '/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/plink_samples.txt'
+    wes_samples_file = '/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/gatk_samples.txt'
     #metadata_file = '/lustre/scratch119/humgen/projects/birth_cohort_wes/genestack/all_samples_with_proceed_and_seq_info_and_warehouse_info.txt'
     metadata_file = '/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/whole_exome_output/sample_info.txt'
     gtcheck_dodgy_samples_file = '/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/whole_exome_output/gtcheck_samples_without_good_hit.txt'
     gtcheck_duplicates_file = '/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/whole_exome_output/gtcheck_duplicates.txt'
     gtcheck_mismatches_file = '/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/whole_exome_output/gtcheck_mismatches.txt'
 
-    plink_samples = find_plink_samples(plinksamples_file)
-    sample_map = parse_metadata(metadata_file, gtcheck_duplicates_file)
+    plink_samples = get_sample_list(plink_samples_file)
+    wes_samples = get_sample_list(wes_samples_file)
+    sample_map = parse_metadata(metadata_file, wes_samples, gtcheck_duplicates_file)
     parse_gtcheck_output(gtcheck_output_file, plink_samples, sample_map,
                          gtcheck_dodgy_samples_file, gtcheck_mismatches_file)
 
