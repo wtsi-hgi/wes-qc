22c3aada2a1af4cc4a76c470b3a672e233fd3bec
RuthEberhardt <re3@sanger.ac.uk>
2022-04-27 13:45:00 +0100

    parse wes vs wes gtcheck

---
+++ check_array_data/check_wes_vs_wes_gtcheck_output.py
@@ -0,0 +1,49 @@
+# take file with the best hit for each sample from gtcheck
+# check that the identifier matches that in sample_apirs.txt and that the score is good (< 0.05)
+# write to an output file
+
+def get_sample_pairs(expected_pairs_file):
+    pairs = {}
+    with open(expected_pairs_file, 'r') as f:
+        lines = f.readlines()
+        for l in lines:
+            ldata = l.split()
+            pairs[ldata[0]] = ldata[1]
+    return pairs
+
+
+def analyse_gtcheck_output(gtcheck_output_file, expected_pairs, output_file):
+    outdata = []
+    with open(gtcheck_output_file, 'r') as f:
+        lines = f.readlines()
+        for l in lines:
+            if l.startswith('#EGA'):
+                outline = l.rstrip() + "\t" + "match_status" + "\t" + "note" + "\n"
+                outdata.append(outline)
+            else:
+                l = l.rstrip()
+                ldata = l.split()
+                match = 'no'
+                if ldata[0] == expected_pairs[ldata[1]]:
+                    match = 'yes'
+                note = ''
+                if float(ldata[4]) > 0.05:
+                    note = 'high_score'
+                outline = l + "\t" + match + "\t" + note + "\n"
+        
+    with open(output_file, 'w') as o:
+        for l in outdata:
+            o.write(l)
+
+
+def main():
+    gtcheck_output_file = '/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/check_wes_vs_wes/gtcheck_top_hits.txt'
+    expected_pairs_file = '/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/check_wes_vs_wes/sample_pairs.txt'
+    output_file = '/lustre/scratch123/hgi/projects/birth_cohort_wes/qc/check_array_genotypes/check_wes_vs_wes/gtcheck_analysis.txt'
+
+    expected_pairs = get_sample_pairs(expected_pairs_file)
+    analyse_gtcheck_output(gtcheck_output_file, expected_pairs, output_file)
+
+
+if __name__ == '__main__':
+    main()
