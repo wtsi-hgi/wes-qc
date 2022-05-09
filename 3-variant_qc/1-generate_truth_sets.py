642d62441ec92184b7b68aa4a599218f1f7a00d9
RuthEberhardt <re3@sanger.ac.uk>
2022-05-09 15:48:10 +0100

    generate truth sets for rf

--- 3-variant_qc/1-generate_truth_sets.py
+++ 3-variant_qc/1-generate_truth_sets.py
@@ -2,7 +2,10 @@
 import hail as hl
 import pyspark
 import argparse
+from typing import Tuple
 from wes_qc.utils.utils import parse_config
+from gnomad.utils.filtering import filter_to_adj
+from gnomad.utils.annotations import unphase_call_expr, bi_allelic_site_inbreeding_expr, add_variant_type, annotate_adj
 
 def get_options():
     '''
@@ -37,6 +40,183 @@ def get_truth_ht(omni, mills, thousand_genomes, hapmap, truth_ht_file):
     truth_ht.write(truth_ht_file, overwrite=True)
     
 
+def split_multi_and_var_qc(mtfile: str, varqc_mtfile: str):
+    '''
+    Taken from https://github.com/broadinstitute/gnomad_qc/blob/3d79bdf0f7049c209b4659ff8c418a1b859d7cfa/gnomad_qc/v2/annotations/generate_qc_annotations.py
+    Run split_multi_hts and variant QC on inout mt after sample QC
+    :param str mtfile: Input mtfile, raw variants with sample QC fails removed
+    :param str varqc_mtfile: Output mt with variant QC annotation and split multiallelics
+    '''
+    mt = hl.read_matrix_table(mtfile)
+    mt = hl.split_multi_hts(mt)
+    mt = hl.variant_qc(mt)
+    mt.write(varqc_mtfile, overwrite=True)
+
+
+def read_fam(fam_file: str) -> hl.Table:
+    '''
+    Taken from https://github.com/broadinstitute/gnomad_qc/blob/3d79bdf0f7049c209b4659ff8c418a1b859d7cfa/gnomad_qc/v2/annotations/generate_qc_annotations.py
+    '''
+    columns = ['fam_id', 's', 'pat_id', 'mat_id', 'is_female']
+    return hl.import_table(fam_file, no_header=True).rename({f'f{i}': c for i, c in enumerate(columns)}).key_by('s')
+
+
+def annotate_unrelated_sample(mt: hl.MatrixTable, fam_file: str) -> hl.MatrixTable:
+    '''
+    Taken from https://github.com/broadinstitute/gnomad_qc/blob/3d79bdf0f7049c209b4659ff8c418a1b859d7cfa/gnomad_qc/v2/annotations/generate_qc_annotations.py
+    '''
+    fam_ht = read_fam(fam_file)
+    return mt.annotate_cols(unrelated_sample=hl.is_missing(fam_ht[mt.s]))
+
+
+def family_stats(mt: hl.MatrixTable, ped: hl.Pedigree, group_name: str) -> Tuple[hl.expr.StructExpression, hl.Table]:
+    '''
+    Taken from https://github.com/broadinstitute/gnomad_qc/blob/3d79bdf0f7049c209b4659ff8c418a1b859d7cfa/gnomad_qc/v2/annotations/generate_qc_annotations.py
+    '''
+    tdt_table = hl.transmission_disequilibrium_test(mt, ped)
+    _, _, per_sample, per_variant = hl.mendel_errors(mt.GT, ped)
+    family_stats_struct = hl.struct(mendel=per_variant[mt.row_key],
+                                    tdt=tdt_table[mt.row_key],
+                                    unrelated_qc_callstats=hl.agg.filter(mt.unrelated_sample,
+                                                                         hl.agg.call_stats(mt.GT, mt.alleles)),
+                                    meta={'group': group_name})
+    return family_stats_struct, per_sample
+
+
+def generate_family_stats(mt: hl.MatrixTable, fam_file: str, calculate_adj: bool = False) -> Tuple[hl.Table, hl.Table]:
+    """
+    Taken from https://github.com/broadinstitute/gnomad_qc/blob/3d79bdf0f7049c209b4659ff8c418a1b859d7cfa/gnomad_qc/v2/annotations/generate_qc_annotations.py
+    Writes bi-allelic sites MT with the following annotations:
+     - family_stats (TDT, Mendel Errors, AC_unrelated_qc)
+     - truth_data (presence in Omni, HapMap, 1KG/TGP high conf SNVs, Mills)
+    :param MatrixTable mt: Full MT
+    :param str fam_file: Fam pedigree file location
+    :param bool calculate_adj: Whether to also calculate family metrics for adj genotypes
+    :return: Table with qc annotations
+    :rtype: Table
+    """
+    mt = mt.select_cols(high_quality=mt.meta.high_quality)
+    mt = mt.select_rows()
+    mt = annotate_unrelated_sample(mt, fam_file)
+
+    # Unphased for now, since mendel_errors does not support phased alleles
+    mt = mt.annotate_entries(GT=unphase_call_expr(mt.GT))
+    ped = hl.Pedigree.read(fam_file, delimiter='\\t')
+    family_stats_struct, family_stats_sample_ht = family_stats(mt, ped, 'raw')
+    mt = mt.annotate_rows(family_stats=[family_stats_struct])
+
+    if calculate_adj:
+        mt = filter_to_adj(mt)
+        adj_family_stats_struct, adj_family_stats_sample_ht = family_stats(mt, ped, 'adj')
+
+        family_stats_sample_ht = family_stats_sample_ht.annotate(
+            adj=adj_family_stats_sample_ht[family_stats_sample_ht.s])
+
+        mt = mt.annotate_rows(family_stats=mt.family_stats.append(adj_family_stats_struct))
+
+    return mt.rows(), family_stats_sample_ht
+
+
+def trio_family_dnm_annotation(varqc_mtfile: str, pedfile: str, trio_mtfile: str, fam_stats_htfile: str, fam_stats_mtfile: str, gnomad_htfile: str, dnm_htfile: str):
+    '''
+    Create matrixtable of just trios, create family stats
+    param str varqc_mtfile: Input matrixtable
+    param str pedfile: Plink-format ped file
+    param str trio_mtfile: Trio matrixtable file
+    param str fam_stats_htfile: Family stats hail table file
+    param str fam_stats_htfile: Family stats matrixtble file
+    param str gnomad_htfile: Gnomad AF hail table
+    param str dnm_htfile: De novo hail table file
+    '''
+    pedigree = hl.Pedigree.read(pedfile)
+    mt = hl.read_matrix_table(varqc_mtfile)
+    trio_dataset = hl.trio_matrix(mt, pedigree, complete_trios=True)
+    trio_dataset.write(trio_mtfile, overwrite=True)
+
+    print("Generating family stats")
+    (ht1, famstats_ht) = generate_family_stats(mt, pedigree)
+    ht1.write(fam_stats_htfile ,overwrite=True)
+
+    mt = mt.annotate_rows(family_stats=ht1[mt.row_key].family_stats)
+    mt=mt.checkpoint(fam_stats_mtfile , overwrite=True)
+    #add gnomad AFs
+    priors = hl.read_table(gnomad_htfile)
+    mt = mt.annotate_rows(gnomad_maf=priors[mt.row_key].maf)
+    mt = mt.checkpoint(fam_stats_mtfile, overwrite=True)
+    #make DNM table
+    de_novo_table = hl.de_novo(mt, pedigree, mt.gnomad_maf)
+    de_novo_table = de_novo_table.key_by('locus', 'alleles').collect_by_key('de_novo_data')
+    de_novo_table.write(dnm_htfile, overwrite=True)
+
+
+def generate_allele_data(mt: hl.MatrixTable) -> hl.Table:
+    """
+    Taken from https://github.com/broadinstitute/gnomad_qc/blob/3d79bdf0f7049c209b4659ff8c418a1b859d7cfa/gnomad_qc/v2/annotations/generate_qc_annotations.py
+    Writes bi-allelic sites MT with the following annotations:
+     - allele_data (nonsplit_alleles, has_star, variant_type, and n_alt_alleles)
+    :param MatrixTable mt: Full unsplit MT
+    :return: Table with allele data annotations
+    :rtype: Table
+    """
+    ht = mt.rows().select()
+    allele_data = hl.struct(nonsplit_alleles=ht.alleles,
+                            has_star=hl.any(lambda a: a == '*', ht.alleles))
+    ht = ht.annotate(allele_data=allele_data.annotate(
+        **add_variant_type(ht.alleles)))
+
+    ht = hl.split_multi_hts(ht)
+    allele_type = (hl.case()
+                   .when(hl.is_snp(ht.alleles[0], ht.alleles[1]), 'snv')
+                   .when(hl.is_insertion(ht.alleles[0], ht.alleles[1]), 'ins')
+                   .when(hl.is_deletion(ht.alleles[0], ht.alleles[1]), 'del')
+                   .default('complex')
+                   )
+    ht = ht.annotate(allele_data=ht.allele_data.annotate(allele_type=allele_type,
+                                                         was_mixed=ht.allele_data.variant_type == 'mixed'))
+    return ht
+
+
+def generate_ac(mt: hl.MatrixTable, fam_file: str) -> hl.Table:
+    """
+    Creates Table with QC samples, QC samples removing children and release samples raw and adj ACs.
+    """
+    # mt = mt.filter_cols(mt.meta.high_quality)
+    fam_ht = hl.import_fam(fam_file, delimiter="\t")
+    mt = mt.annotate_cols(unrelated_sample=hl.is_missing(fam_ht[mt.s]))
+    mt = mt.filter_rows(hl.len(mt.alleles) > 1)
+    mt = annotate_adj(mt)
+    mt = mt.annotate_rows(
+        ac_qc_samples_raw=hl.agg.sum(mt.GT.n_alt_alleles()),
+        ac_qc_samples_adj=hl.agg.filter(
+            mt.adj, hl.agg.sum(mt.GT.n_alt_alleles())),
+    )
+    return mt.rows()
+
+
+def create_inbreeding_ht_with_ac_and_allele_data(varqc_mtfile: str, pedfile: str, inbreeding_htfile: str, qc_ac_htfile: str, allele_data_htfile: str):
+    '''
+    Inbreeding, allele data and qc_ac annotations
+    param str pedfile: Plink-format ped file
+    param str varqc_mtfile: Input matrixtable
+    param str inbreeding_htfile: Inbreeding hail table file
+    param str qc_ac_htfile: qc_ac hail table file
+    param str allele_data_htfile: Allele data htfile
+    '''
+    mt = hl.read_matrix_table(varqc_mtfile)
+    pedigree = hl.Pedigree.read(pedfile)
+    # inbreeding ht
+    mt_inbreeding = mt.annotate_rows(InbreedingCoeff=bi_allelic_site_inbreeding_expr(mt.GT))
+    mt = mt.key_rows_by('locus').distinct_by_row().key_rows_by('locus', 'alleles')
+    ht_inbreeding = mt_inbreeding.rows()
+    # allele data and qc_ac ht
+    allele_data_ht = generate_allele_data(mt)
+    qc_ac_ht = generate_ac(mt, pedigree)
+    # write to file
+    ht_inbreeding.write(inbreeding_htfile , overwrite=True)
+    qc_ac_ht.write(qc_ac_htfile , overwrite=True)
+    allele_data_ht.write(allele_data_htfile, overwrite=True)
+
+
 def main():
     #set up
     args = get_options()
@@ -53,6 +233,26 @@ def main():
         thousand_genomes = training_sets_dir + "1000G_phase1.snps.high_confidence.hg38.ht"
         hapmap = training_sets_dir + "hapmap_3.3.hg38.ht"
         get_truth_ht(omni, mills, thousand_genomes, hapmap, truth_ht_file)
+
+    #add hail variant QC
+    mtfile = mtdir + "mt_pops_QC_filters_sequencing_location_and_superpop_sanger_only_after_sample_qc.mt"
+    varqc_mtfile = mtdir + "mt_varqc_splitmulti.mt"
+    split_multi_and_var_qc(mtfile, varqc_mtfile)
+
+    #get complete trios, family annotation, dnm annotation
+    pedfile = resourcedir + "trios.ped"
+    trio_mtfile = mtdir + "trios.mt"
+    fam_stats_htfile = mtdir + "family_stats.ht"
+    fam_stats_mtfile = mtdir + "family_stats.mt"
+    gnomad_htfile = resourcedir + "gnomad_v3-0_AF.ht"
+    dnm_htfile = mtdir + "denovo_table.ht"
+    trio_family_dnm_annotation(varqc_mtfile, pedfile, trio_mtfile, fam_stats_htfile, fam_stats_mtfile, gnomad_htfile, dnm_htfile)
+
+    #create inbreeding ht
+    inbreeding_htfile = mtdir + "inbreeding.ht"
+    qc_ac_htfile = mtdir + "qc_ac.ht"
+    allele_data_htfile = mtdir + "allele_data.ht"
+    create_inbreeding_ht_with_ac_and_allele_data(varqc_mtfile, pedfile, inbreeding_htfile, qc_ac_htfile, allele_data_htfile)
 
 if __name__ == '__main__':
     main()
