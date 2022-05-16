55762e04616ae87953dae391348827f831f8ed4b
RuthEberhardt <re3@sanger.ac.uk>
2022-05-16 09:25:10 +0100

    print runs.json - can it print without the feature importance and test results?

--- 3-variant_qc/3-train_rf.py
+++ 3-variant_qc/3-train_rf.py
@@ -155,8 +155,8 @@ def main():
         transmitted_singletons=True,
         test_intervals=test_interval,
         adj=True,
-        features_importance=hl.eval(ht_result.features_importance),
-        test_results=hl.eval(ht_result.test_results),
+        #features_importance=hl.eval(ht_result.features_importance),
+        #test_results=hl.eval(ht_result.test_results),
     )
 
     with hl.hadoop_open(runs_json, "w") as f:
