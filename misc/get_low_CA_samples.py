190a49abc3f8b90735bdd3bf179a80a4e212222b
RuthEberhardt <re3@sanger.ac.uk>
2022-07-06 15:14:54 +0100

    relax filter to allow through more samples

--- misc/get_low_CA_samples.py
+++ misc/get_low_CA_samples.py
@@ -1,6 +1,6 @@
-#filter the ALSPAC data to just samples with fraction CA < 0.08
-#this is a stringent filter, the throey is that the high CA samples are creating excessive FP variants in variant QC
-#using a very strict CA filter to test this
+#filter the ALSPAC data to just samples with fraction CA < 0.1
+#the throey is that the high CA samples are creating excessive FP variants in variant QC
+#using a CA filter to test this
 
 import hail as hl
 import pyspark
@@ -18,7 +18,7 @@ def main():
     spec_df = pd.read_csv(spectrafile ,sep="\t")
     spec_df['CA'] = spec_df['C>A'] + spec_df['G>T']#C>A and G>T both count to the total fraction C>A
     spectra_ht = hl.Table.from_pandas(spec_df).key_by('sample')
-    spectra_ht = spectra_ht.filter(spectra_ht.CA < 0.08)#filter to only low CA samples
+    spectra_ht = spectra_ht.filter(spectra_ht.CA < 0.1)#filter to only low CA samples
     #load in full MT
     mtfile = mtdir + "mt_pops_QC_filters_sequencing_location_and_superpop_sanger_only_after_sample_qc.mt"
     mt = hl.read_matrix_table(mtfile)
