922dfcfa49162fdf53f85c84522515c079aab778
RuthEberhardt <re3@sanger.ac.uk>
2022-07-12 11:10:44 +0100

    run tdt test on all varas

--- misc/run_transmission_disequilibrium_test.py
+++ misc/run_transmission_disequilibrium_test.py
@@ -26,7 +26,13 @@ def main():
     mtdir = inputs['matrixtables_lustre_dir']
     resourcedir = inputs['resource_dir']
 
-    varqc_mtfile = mtdir + "mt_varqc_splitmulti.mt"
+    #initialise hail
+    tmp_dir = "hdfs://spark-master:9820/"
+    sc = pyspark.SparkContext()
+    hadoop_config = sc._jsc.hadoopConfiguration()
+    hl.init(sc=sc, tmp_dir=tmp_dir, default_reference="GRCh38")
+
+    varqc_mtfile = mtdir + "/mts_from_40eedd2d/mt_varqc_splitmulti.mt"#use file from a run of variant QC where no variants exlcuded
     pedfile = resourcedir + "trios.ped"
     gnomad_htfile = resourcedir + "gnomad_v3-0_AF.ht"
     tdt_htfile = mtdir + "tansmission_disequilibrium_test.ht"
