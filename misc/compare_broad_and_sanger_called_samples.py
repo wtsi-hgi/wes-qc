a41a46af79e70e2da5d9a86e1d041d6662ea6932
RuthEberhardt <re3@sanger.ac.uk>
2022-08-09 09:50:58 +0100

    save split mt interim file

--- misc/compare_broad_and_sanger_called_samples.py
+++ misc/compare_broad_and_sanger_called_samples.py
@@ -39,8 +39,8 @@ def main():
     #save split multi and reload
     broad_split_multi_mtfile = mtdir + "gatk_calls_from_broad_split_multi.mt"
     sanger_split_multi_mtfile = mtdir + "gatk_unprocessed_split_multi.mt"
-    broad_mt_split.write(broad_split_multi_mtfile)
-    sanger_mt_split.write(sanger_split_multi_mtfile)
+    broad_mt_split.write(broad_split_multi_mtfile, overwrite = True)
+    sanger_mt_split.write(sanger_split_multi_mtfile, overwrite = True)
     sanger_mt = hl.read_matrix_table(sanger_split_multi_mtfile)
     broad_mt = hl.read_matrix_table(broad_split_multi_mtfile)
     #remove any that are only ref alleles
