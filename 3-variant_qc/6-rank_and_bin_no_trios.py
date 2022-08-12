ef7fa21ec8e87ae9227eb4c6483c29dba3d26964
RuthEberhardt <re3@sanger.ac.uk>
2022-08-12 11:11:39 +0100

    cant rank transmitted singletons

--- 3-variant_qc/6-rank_and_bin_no_trios.py
+++ 3-variant_qc/6-rank_and_bin_no_trios.py
@@ -284,9 +284,9 @@ def main():
     ht_ranked = add_rank(ht,
                         score_expr=(1-ht.rf_probability["TP"]),
                         subrank_expr={
-                            'singleton_rank': ht.transmitted_singleton,
+                            # 'singleton_rank': ht.transmitted_singleton,
                             'biallelic_rank': ~ht.was_split,
-                            'biallelic_singleton_rank': ~ht.was_split & ht.transmitted_singleton,
+                            # 'biallelic_singleton_rank': ~ht.was_split & ht.transmitted_singleton,
                             'de_novo_high_quality_rank': ht.de_novo_data.p_de_novo[0] > 0.9,
                             'de_novo_medium_quality_rank': ht.de_novo_data.p_de_novo[0] > 0.5,
                             'de_novo_synonymous_rank': ht.consequence == "synonymous_variant",
