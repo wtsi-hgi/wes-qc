9291f9f44e0f9e81cdacd9b5e66c5ead3fb4ad67
RuthEberhardt <re3@sanger.ac.uk>
2022-09-26 09:39:42 +0100

    remove subranks

--- 3-variant_qc/6-rank_and_bin.py
+++ 3-variant_qc/6-rank_and_bin.py
@@ -165,13 +165,13 @@ def create_binned_data_initial(ht: hl.Table, bin_tmp_htfile: str, truth_htfile: 
             rank_id=ht.rank_id,
             contig=ht.locus.contig,
             snv=hl.is_snp(ht.alleles[0], ht.alleles[1]),
-            bi_allelic=hl.is_defined(ht.biallelic_rank),
+            # bi_allelic=hl.is_defined(ht.biallelic_rank),
             singleton=ht.transmitted_singleton,
-            trans_singletons=hl.is_defined(ht.singleton_rank),
-            de_novo_high_quality=ht.de_novo_high_quality_rank,
-            de_novo_medium_quality=hl.is_defined(
-                ht.de_novo_medium_quality_rank),
-            de_novo_synonymous=hl.is_defined(ht.de_novo_synonymous_rank),
+            # trans_singletons=hl.is_defined(ht.singleton_rank),
+            # de_novo_high_quality=ht.de_novo_high_quality_rank,
+            # de_novo_medium_quality=hl.is_defined(
+            #     ht.de_novo_medium_quality_rank),
+            # de_novo_synonymous=hl.is_defined(ht.de_novo_synonymous_rank),
             # release_adj=ht.ac > 0,
             bin=ht.bin
         )._set_buffer_size(20000)
