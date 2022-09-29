34f1caf4404fb9b2b9ae4e83959a7497750c4f96
RuthEberhardt <re3@sanger.ac.uk>
2022-09-29 08:36:04 +0100

    mtfile is genotype QCed file

--- variant_counts_per_cq_controls/count_vars_in_mt.py
+++ variant_counts_per_cq_controls/count_vars_in_mt.py
@@ -6,6 +6,8 @@ import pyspark
 from wes_qc.utils.utils import parse_config
 from wes_qc.variant_counts_per_cq_controls.annotation_functions import annotate_cq, annotate_gnomad, get_counts_per_cq
 
+
+
 def main():
     # set up
     inputs = parse_config()
@@ -18,15 +20,11 @@ def main():
     hadoop_config = sc._jsc.hadoopConfiguration()
     hl.init(sc=sc, tmp_dir=tmp_dir, default_reference="GRCh38")
 
-    broad_mtfile = mtdir + "gatk_calls_from_broad.mt"
-    broad_mt = hl.read_matrix_table(broad_mtfile)
-    broad_mt_split = hl.split_multi_hts(broad_mt)
-    broad_split_multi_mtfile = mtdir + "gatk_calls_from_broad_split_multi_tmp.mt"
-    broad_mt_split.write(broad_split_multi_mtfile, overwrite = True)
-    broad_mt = hl.read_matrix_table(broad_split_multi_mtfile)
+    mtfile = mtdir + "mt_after_var_qc_hard_filter_gt.mt"
+    mt = hl.read_matrix_table(mtfile)
 
     cqfile = "file:///lustre/scratch123/qc/compare_broad_sanger_vcfs/broad_vcf_samples_in_sanger_filter_by_bait/split_multi_strip_gt/all_consequences.txt"
-    mtcq = annotate_cq(broad_mt, cqfile)
+    mtcq = annotate_cq(mt, cqfile)
     gnomad_htfile = resourcedir + "gnomad.exomes.r2.1.1.sites.liftover_grch38.ht"
     mtgnomad = annotate_gnomad(mtcq, gnomad_htfile)
 
