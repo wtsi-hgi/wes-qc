0c0b4eaaf65156dcb2975374c4a3e27c6d3f5a8c
RuthEberhardt <re3@sanger.ac.uk>
2022-10-04 14:18:07 +0100

    rewrite to create files for variants per sample per cq and ca fraction

--- 4-genotype_qc/2-counts_per_sample.py
+++ 4-genotype_qc/2-counts_per_sample.py
@@ -1,4 +1,4 @@
-# median counts of variants of each consequence per sample plus transmitted/untransmitted 
+# median counts of variants of each consequence per sample plus transmitted/untransmitted
 # ratio for synonymous singletons
 import hail as hl
 import pyspark
@@ -22,7 +22,7 @@ def annotate_gnomad(mt_in: hl.MatrixTable, gnomad_htfile: str) -> hl.MatrixTable
     mt_in = mt_in.annotate_rows(gnomad_AC=gnomad_ht[mt_in.row_key].freq[0].AC)
 
     return mt_in
-    
+
 
 def get_trans_untrans_synon_singleton_counts(mt_in: hl.MatrixTable, pedfile: str):
     '''
@@ -31,11 +31,11 @@ def get_trans_untrans_synon_singleton_counts(mt_in: hl.MatrixTable, pedfile: str
     :param str pedfile: Path to pedfile
     '''
     pedigree = hl.Pedigree.read(pedfile)
-    #get trios only then filter to trioAC == 1 or 2
+    # get trios only then filter to trioAC == 1 or 2
     trio_sample_ht = hl.import_fam(pedfile)
     sample_list = trio_sample_ht.id.collect() + trio_sample_ht.pat_id.collect() + trio_sample_ht.mat_id.collect()
     mt2 = mt_in.filter_cols(hl.set(sample_list).contains(mt_in.s))
-    mt2 = hl.variant_qc(mt2, name = 'varqc_trios')
+    mt2 = hl.variant_qc(mt2, name='varqc_trios')
     mt2 = mt2.filter_rows(mt2.varqc_trios.AC[1] <= 2)
 
     synon_mt = mt2.filter_rows(mt2.info.consequence == 'synonymous_variant')
@@ -51,39 +51,77 @@ def get_trans_untrans_synon_singleton_counts(mt_in: hl.MatrixTable, pedfile: str
     untrans = untrans_sing.count()
     ratio = trans/untrans
 
-    print("There are " + str(trans) + " transmitted synonymous singletons and " + str(untrans) + " untransmitted synonymous singletons")
-    print("The ratio of transmitted/unstransmitted singletons is " +'{0:.4f}'.format(ratio) )
+    print("There are " + str(trans) + " transmitted synonymous singletons and " +
+          str(untrans) + " untransmitted synonymous singletons")
+    print("The ratio of transmitted/unstransmitted singletons is " + '{0:.4f}'.format(ratio))
 
 
-def get_counts_per_cq(mt_in: hl.MatrixTable):
+def get_counts_per_cq(mt_in: hl.MatrixTable, outfile: str):
     '''
     Get median counts of each consequence per sample
     :param hl.MatrixTable mt_in: Input MatrixTable
+    :param str outfile: output text file path
     '''
-    #split mt by snvs and indels
+    # split mt by snvs and indels
     snv_mt = mt_in.filter_rows(hl.is_snp(mt_in.alleles[0], mt_in.alleles[1]))
     indel_mt = mt_in.filter_rows(hl.is_indel(mt_in.alleles[0], mt_in.alleles[1]))
 
-    #get median numbers of variants by consequence
-    synonymous_counts =  median_count_for_cq(snv_mt, ['synonymous_variant'])
-    missense_counts =  median_count_for_cq(snv_mt, ['missense_variant'])
-    nonsense_counts =  median_count_for_cq(snv_mt, ['stop_gained'])
-    splice_acc_don_counts = median_count_for_cq(snv_mt, ['splice_acceptor_variant', 'splice_donor_variant'])
+    # #get median numbers of variants by consequence
+    # synonymous_counts =  median_count_for_cq(snv_mt, ['synonymous_variant'])
+    # missense_counts =  median_count_for_cq(snv_mt, ['missense_variant'])
+    # nonsense_counts =  median_count_for_cq(snv_mt, ['stop_gained'])
+    # splice_acc_don_counts = median_count_for_cq(snv_mt, ['splice_acceptor_variant', 'splice_donor_variant'])
 
-    frameshift_counts =  median_count_for_cq(indel_mt, ['frameshift_variant'])
-    inframe_indel_counts = median_count_for_cq(indel_mt, ['inframe_deletion', 'inframe_insertion'])
+    # frameshift_counts =  median_count_for_cq(indel_mt, ['frameshift_variant'])
+    # inframe_indel_counts = median_count_for_cq(indel_mt, ['inframe_deletion', 'inframe_insertion'])
 
-    coding_snv_counts = median_count_for_cq(snv_mt, ['synonymous_variant', 'missense_variant', 'stop_gained','splice_acceptor_variant', 'splice_donor_variant', 'sart_lost', 'stop_lost'])
-    coding_indel_counts = median_count_for_cq(indel_mt, ['frameshift_variant', 'inframe_deletion', 'inframe_insertion'])
+    # coding_snv_counts = median_count_for_cq(snv_mt, ['synonymous_variant', 'missense_variant', 'stop_gained','splice_acceptor_variant', 'splice_donor_variant', 'sart_lost', 'stop_lost'])
+    # coding_indel_counts = median_count_for_cq(indel_mt, ['frameshift_variant', 'inframe_deletion', 'inframe_insertion'])
 
-    print("Synonymous: Total " + str(synonymous_counts[0]) + " rare " + str(synonymous_counts[1]))
-    print("Missense: Total " + str(missense_counts[0]) + " rare " + str(missense_counts[1]))
-    print("Nonsense: Total " + str(nonsense_counts[0]) + " rare " + str(nonsense_counts[1]))
-    print("Splicing: Total " + str(splice_acc_don_counts[0]) + " rare " + str(splice_acc_don_counts[1]))
-    print("Frameshift: Total " + str(frameshift_counts[0]) + " rare " + str(frameshift_counts[1]))
-    print("In-frame indel: Total " + str(inframe_indel_counts[0]) + " rare " + str(inframe_indel_counts[1]))
-    print("Coding SNV: Total " + str(coding_snv_counts[0]) + " rare " + str(coding_snv_counts[1]))
-    print("Coding indel: Total " + str(coding_indel_counts[0]) + " rare " + str(coding_indel_counts[1]))
+    # print("Synonymous: Total " + str(synonymous_counts[0]) + " rare " + str(synonymous_counts[1]))
+    # print("Missense: Total " + str(missense_counts[0]) + " rare " + str(missense_counts[1]))
+    # print("Nonsense: Total " + str(nonsense_counts[0]) + " rare " + str(nonsense_counts[1]))
+    # print("Splicing: Total " + str(splice_acc_don_counts[0]) + " rare " + str(splice_acc_don_counts[1]))
+    # print("Frameshift: Total " + str(frameshift_counts[0]) + " rare " + str(frameshift_counts[1]))
+    # print("In-frame indel: Total " + str(inframe_indel_counts[0]) + " rare " + str(inframe_indel_counts[1]))
+    # print("Coding SNV: Total " + str(coding_snv_counts[0]) + " rare " + str(coding_snv_counts[1]))
+    # print("Coding indel: Total " + str(coding_indel_counts[0]) + " rare " + str(coding_indel_counts[1]))
+
+    # get variant counts per consequence
+    synonymous_all, synonymous_rare = counts_per_cq(snv_mt, ['synonymous_variant'])
+    missense_all, missense_rare = counts_per_cq(snv_mt, ['missense_variant'])
+    nonsense_all, nonsense_rare = counts_per_cq(snv_mt, ['stop_gained'])
+    splice_all, splice_rare = counts_per_cq(snv_mt, ['splice_acceptor_variant', 'splice_donor_variant'])
+
+    frameshift_all, frameshift_rare = counts_per_cq(indel_mt, ['frameshift_variant'])
+    inframe_all, inframe_rare = counts_per_cq(indel_mt, ['inframe_deletion', 'inframe_insertion'])
+
+    coding_snv_all, coding_snv_rare = counts_per_cq(
+        snv_mt, ['synonymous_variant', 'missense_variant', 'stop_gained', 'splice_acceptor_variant', 
+        'splice_donor_variant', 'sart_lost', 'stop_lost'])
+    coding_indel_all, coding_indel_rare = counts_per_cq(
+        indel_mt, ['frameshift_variant', 'inframe_deletion', 'inframe_insertion'])
+
+    # print to output table
+    outdata = list(zip(synonymous_all, synonymous_rare, missense_all, missense_rare, nonsense_all, nonsense_rare, 
+        splice_all, splice_rare, frameshift_all, frameshift_rare, inframe_all, inframe_rare, coding_snv_all, 
+        coding_snv_rare, coding_indel_all, coding_indel_rare))
+
+    header = ['synonymous_all', 'synonymous_rare', 'missense_all', 'missense_rare', 'nonsense_all', 
+        'nonsense_rare', 'splice_all', 'splice_rare', 'frameshift_all', 'frameshife_rare', 'in_frame_all', 
+        'in_frame_rare', 'coding_snv_all', 'coding_snv_rare', 'coding_indel_all', 'coding_indel_rare']
+
+    n_rows = len(synonymous_all)
+    n_cols = len(header)
+    with open(outfile, 'w') as o:
+        o.write(("\t").join(header))
+        o.write("\n")
+        for i in range(0, n_rows):
+            linedata = []
+            for j in range(0, n_cols):
+                linedata.append(str(outdata[j][i]))
+            o.write(("\t").join(linedata))
+            o.write("\n")
 
 
 def median_count_for_cq(mt_in: hl.MatrixTable, cqs: list) -> tuple:
@@ -103,24 +141,90 @@ def median_count_for_cq(mt_in: hl.MatrixTable, cqs: list) -> tuple:
     sampleqc_rare_ht = mt_rare.cols()
     total_median = hl.median(sampleqc_ht.sample_qc.n_non_ref.collect()).collect()[0]
     rare_median = hl.median(sampleqc_rare_ht.sample_qc.n_non_ref.collect()).collect()[0]
-    
+
     return total_median, rare_median
 
 
-def get_median_ca_fraction(mt_in: hl.MatrixTable):
+def counts_per_cq(mt_in: hl.MatrixTable, cqs: list) -> tuple:
+    '''
+    Get all counts per sample for a list of consequences
+    :param hl.MatrixTable mt_in: Input MatrixTable
+    :param list cqs: List fof consequences
+    :return: dict
+    '''
+    mt = mt_in.filter_rows(hl.literal(cqs).contains(mt_in.info.consequence))
+    mt_rare = mt.filter_rows(mt.gnomad_AC < 5)
+    mt = hl.sample_qc(mt)
+    mt_rare = hl.sample_qc(mt_rare)
+    sampleqc_ht = mt.cols()
+    sampleqc_rare_ht = mt_rare.cols()
+    counts_all = sampleqc_ht.n_non_ref.collect()
+    counts_rare = sampleqc_rare_ht.n_non_ref.collect()
+
+    return counts_all, counts_rare
+
+
+def get_median_ca_fraction(mt_in: hl.MatrixTable, outfile: str):
     '''
     Get fraction CA per sample
     :param hl.MatrixTable mt_in: Input MatrixTable
+    :param str outfile: output text file path
     '''
-    #filter to SNVs and to rare
+    # filter to SNVs and to rare
     snv_mt = mt_in.filter_rows(hl.is_snp(mt_in.alleles[0], mt_in.alleles[1]))
     snv_mt_rare = snv_mt.filter_rows(snv_mt.gnomad_AC < 5)
 
-    total_ca_frac = get_median_ca_per_sample(snv_mt)
-    rare_ca_frac = get_median_ca_per_sample(snv_mt_rare)
+    # total_ca_frac = get_median_ca_per_sample(snv_mt)
+    # rare_ca_frac = get_median_ca_per_sample(snv_mt_rare)
+    total_ca_frac = get_ca_per_sample(snv_mt)
+    rare_ca_frac = get_ca_per_sample(snv_mt_rare)
 
-    print("Median fraction CA per sample total: " + "{:.5f}".format(total_ca_frac))
-    print("Median fraction CA per sample rare: " + "{:.5f}".format(rare_ca_frac))
+    # print("Median fraction CA per sample total: " + "{:.5f}".format(total_ca_frac))
+    # print("Median fraction CA per sample rare: " + "{:.5f}".format(rare_ca_frac))
+    outdata = list(zip(total_ca_frac, rare_ca_frac))
+
+    header = ['total_ca_fraction', 'rare_ca_fraction']
+    n_rows = len(outdata)
+    n_cols = len(header)
+
+    with open(outfile, 'w') as o:
+        o.write(("\t").join(header))
+        o.write("\n")
+        for i in range(0, n_rows):
+            linedata = []
+            for j in range(0, n_cols):
+                n = "{:.5f}".format(outdata[j][i])
+                linedata.append(n)
+            o.write(("\t").join(linedata))
+            o.write("\n")
+
+
+def get_ca_per_sample(mt_in: hl.MatrixTable) -> list:
+    '''
+    Get fraction CA for each sample in a matrixtable
+    :param hl.MatrixTable mt_in: Input MatrixTable
+    :return: list of C>A fractions
+    '''
+    mt_in = mt_in.annotate_rows(is_CA=((mt_in.alleles[0] == "C") & (mt_in.alleles[1] == "A")) | (
+        (mt_in.alleles[0] == "G") & (mt_in.alleles[1] == "T")))
+    ca_mt = mt_in.filter_rows(mt_in.is_CA == True)
+    # run sample qc and extract cols
+    mt_in = hl.sample_qc(mt_in)
+    ca_mt = hl.sample_qc(ca_mt)
+    snv_qc = mt_in.cols()
+    ca_qc = ca_mt.cols()
+
+    snv_count = snv_qc.sample_qc.n_non_ref.collect()
+    ca_count = ca_qc.sample_qc.n_non_ref.collect()
+    fracs = []
+    for i in (0, len(snv_count)):
+        if snv_count[i] > 0:
+            f = ca_count[i]/snv_count[i]
+            fracs.append(f)
+        else:
+            fracs.append(0.0)
+
+    return fracs
 
 
 def get_median_ca_per_sample(mt_in: hl.MatrixTable) -> float:
@@ -128,11 +232,12 @@ def get_median_ca_per_sample(mt_in: hl.MatrixTable) -> float:
     Get fraction CA per sample
     :param hl.MatrixTable mt_in: Input MatrixTable
     :return: float
-    ''' 
-    #annotate SNVs with is_CA
-    mt_in = mt_in.annotate_rows(is_CA=((mt_in.alleles[0] == "C") & (mt_in.alleles[1] == "A")) | ((mt_in.alleles[0] == "G") & (mt_in.alleles[1] == "T")))
-    ca_mt = mt_in.filter_rows(mt_in.is_CA==True)
-    #run sample qc and extract cols
+    '''
+    # annotate SNVs with is_CA
+    mt_in = mt_in.annotate_rows(is_CA=((mt_in.alleles[0] == "C") & (mt_in.alleles[1] == "A")) | (
+        (mt_in.alleles[0] == "G") & (mt_in.alleles[1] == "T")))
+    ca_mt = mt_in.filter_rows(mt_in.is_CA == True)
+    # run sample qc and extract cols
     mt_in = hl.sample_qc(mt_in)
     ca_mt = hl.sample_qc(ca_mt)
     snv_qc = mt_in.cols()
@@ -165,6 +270,7 @@ def main():
     inputs = parse_config()
     mtdir = inputs['matrixtables_lustre_dir']
     resourcedir = inputs['resource_dir']
+    plot_dir = inputs['plots_dir_local']
 
     # initialise hail
     tmp_dir = "hdfs://spark-master:9820/"
@@ -181,9 +287,11 @@ def main():
     pedfile = resourcedir + "trios.ped"
     get_trans_untrans_synon_singleton_counts(mt, pedfile)
 
-    get_counts_per_cq(mt)
-    get_median_ca_fraction(mt)
+    cqfile = plot_dir + "/variant_counts_per_cq_post_qc.txt"
+    cafile = plot_dir + "/frac_ca_per_sample_post_qc.txt"
 
+    get_counts_per_cq(mt, cqfile)
+    get_median_ca_fraction(mt, cafile)
 
 
 if __name__ == '__main__':
