3768955bc6bf13314e44b86c0393ab21679cd60f
RuthEberhardt <re3@sanger.ac.uk>
2022-10-10 12:01:10 +0100

    get precision and recall

--- evaluation/precision_recall.py
+++ evaluation/precision_recall.py
@@ -44,9 +44,65 @@ def prepare_giab_ht(giab_vcf: str) -> hl.Table:
     :return: hl.Table
     '''
     mt = hl.import_vcf(giab_vcf, force_bgz = True, reference_genome='GRCh38')
-    cts = mt.count()
-    print(cts)
-    exit(0)
+    mt = hl.variant_qc(mt)
+    mt = mt.filter_rows(mt.variant_qc.n_non_ref > 0)
+
+    giab_vars = mt.rows()
+    return giab_vars
+
+
+def get_precision_recall(giab_vars, alspac_vars) -> tuple:
+    '''
+    Get precision and recall for two sets of variants, reference set (GIAB) and test set (ALSPAC)
+    :param hl.Table giab_vars: GIAB variants (reference set)
+    :param hl.Table alspac_vars: ALSPAC variants (test set)
+    :return: tuple
+    '''
+    vars_in_both = giab_vars.semi_join(alspac_vars)
+    giab_only = giab_vars.anti_join(alspac_vars)
+    alspac_only = alspac_vars.anti_join(giab_vars)
+
+    TP = vars_in_both.count()
+    FN = giab_only.count()
+    FP = alspac_only.count()
+
+    precision = TP/(TP + FP)
+    recall = TP / (TP + FN)
+
+    return precision, recall
+
+
+def calculate_precision_recall(alspac_ht, giab_ht) -> dict:
+    '''
+    Calculate precision and recall for each RF bin for both SNPs and indels
+    :param hl.Table alspace_ht: ALSPAC variants hail Table
+    :param hl.Table giab_ht: GIAB variants hail Table
+    :return: dict 
+    '''
+    results = {'snv':{}, 'indel':{}}
+    n_bins = 102 #there are 101 bins but use 102 for range to work
+
+    giab_snvs = giab_ht.filter(hl.is_snp(giab_ht.alleles[0], giab_ht.alleles[1]))
+    giab_indels = giab_ht.filter(hl.is_indel(giab_ht.alleles[0], giab_ht.alleles[1]))
+
+    for bin in range(1,n_bins):
+        results['snv'][bin] = {}
+        results['indel'][bin] = {}
+        alspac_filtered_ht = alspac_ht.filter(alspac_ht.info.bin <= bin)
+        alspac_var_count = alspac_filtered_ht.count()
+        print("bin = " + str(bin) + " n vars = " + str(alspac_var_count))#for sanity check
+        alspac_snvs = alspac_filtered_ht.filter(hl.is_snp(alspac_filtered_ht.alleles[0], alspac_filtered_ht.alleles[1]))
+        alspac_indels = alspac_filtered_ht.filter(hl.is_indel(alspac_filtered_ht.alleles[0], alspac_filtered_ht.alleles[1]))
+
+        snv_prec, snv_recall = get_precision_recall(giab_snvs, alspac_snvs)
+        indel_prec, indel_recall = get_precision_recall(giab_indels, alspac_indels)
+
+        results['snv'][bin]['precision'] = snv_prec
+        results['snv'][bin]['recall'] = snv_recall
+        results['indel'][bin]['precision'] = indel_prec
+        results['indel'][bin]['recall'] = indel_recall
+
+    return results
 
 
 def main():
@@ -71,6 +127,9 @@ def main():
     giab_vcf = resourcedir + "HG001_GRCh38_benchmark.interval.illumina.vcf.gz"
 
     giab_ht = prepare_giab_ht(giab_vcf)
+
+    results = calculate_precision_recall(alspac_vars_ht, giab_ht)
+    print(results)
 
 
 if __name__ == '__main__':
