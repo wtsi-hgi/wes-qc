c87b92366aa5b2d41388643be6e1a668b1c493da
RuthEberhardt <re3@sanger.ac.uk>
2022-10-25 15:43:25 +0100

    colour by bin, symbols for DP, plots from scaled down analysis

--- evaluation/plot_hard_filter_combinations.py
+++ evaluation/plot_hard_filter_combinations.py
@@ -13,8 +13,11 @@ def make_plot(df: pd.DataFrame, x: str, y:str, outfile: str, vartype: str):
     :param str outfile: plot output file
     :param str vartype: Variant type
     '''
-    plottitle = (" ").join([vartype, x, y])
-    fig = px.scatter(df, x=x, y=y, color = "bin", hover_data = ['DP', 'GQ', 'AB'], title = plottitle)
+    if x == "t_u_ratio":
+        plottitle = (" ").join([vartype, "transmitted/untransmitted synonymous singletons", y])
+    else:
+        plottitle = (" ").join([vartype, x, y])
+    fig = px.scatter(df, x=x, y=y, color = "bin", symbol = 'DP', hover_data = ['GQ', 'AB'], title = plottitle)
     fig.write_html(outfile)
 
 def create_plots(snv_results_file: str, indel_results_file: str, outdir: str):
@@ -34,8 +37,24 @@ def create_plots(snv_results_file: str, indel_results_file: str, outdir: str):
     
     snv_tp_fp_file = outdir + "snv_tp_fp.html"
     make_plot(snv_df, "TP", "FP", snv_tp_fp_file, 'SNV')
+
+    snv_tu_tp_file = outdir + "snv_tu_tp.html"
+    make_plot(snv_df, "t_u_ratio", "TP", snv_tu_tp_file, 'SNV')
+
+    snv_prec_recall_file = outdir + "snv_prec_recall.html"
+    make_plot(snv_df, "recall", "precision", snv_prec_recall_file, 'SNV')
+
     indel_tp_fp_file = outdir + "indel_tp_fp.html"
     make_plot(indel_df, "TP", "FP", indel_tp_fp_file, 'Indel')
+
+    indel_prec_recall_file = outdir + "indel_prec_recall.html"
+    make_plot(indel_df, "recall", "precision", indel_prec_recall_file, 'Indel')
+
+    indel_prec_recall_file_frameshift = outdir + "indel_prec_recall_frameshift.html"
+    make_plot(indel_df, "recall_frameshift", "precision_frameshift", indel_prec_recall_file_frameshift, 'Indel')
+
+    indel_prec_recall_file_inframe = outdir + "indel_prec_recall_inframe.html"
+    make_plot(indel_df, "recall_inframe", "precision_inframe", indel_prec_recall_file_inframe, 'Indel')
 
 
 def main():
@@ -43,8 +62,10 @@ def main():
     inputs = parse_config()
     plot_dir = inputs['plots_dir_local']
 
-    snv_results_file = plot_dir + "genotype_hard_filter_comparison_snv_v1.txt"
-    indel_results_file = plot_dir + "genotype_hard_filter_comparison_indel_v1.txt"
+    # snv_results_file = plot_dir + "genotype_hard_filter_comparison_snv_v1.txt"
+    # indel_results_file = plot_dir + "genotype_hard_filter_comparison_indel_v1.txt"
+    snv_results_file = plot_dir + "genotype_hard_filter_comparison_snv_fewer_combs.txt"
+    indel_results_file = plot_dir + "genotype_hard_filter_comparison_indel_fewer_combs.txt"
     outdir = plot_dir + "hard_filter_evaluation/"
 
     create_plots(snv_results_file, indel_results_file, outdir)
