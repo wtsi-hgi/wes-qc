517601883bd7688ccf42fb816c214109f235cd3d
RuthEberhardt <re3@sanger.ac.uk>
2022-11-01 14:13:34 +0000

    filter by RF and genotypes and write to VCF

---
+++ 4-genotype_qc/1a-apply_range_of_hard_filters.py
@@ -0,0 +1,177 @@
+#apply a range of different hard filters (RF bin and genotype) to SNPs and indels, also add gene and consequence
+import hail as hl
+import pyspark
+import argparse
+from wes_qc.utils.utils import parse_config
+
+
+def get_options():
+    '''
+    Get options from the command line
+    '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--runhash", help="RF run hash")
+    args = parser.parse_args()
+    if not args.runhash:
+        print("--runhash must be specified")
+        exit(1)
+
+    return args
+
+
+def annotate_cq_rf(mt: hl.MatrixTable, rf_htfile: str, cqfile: str) -> hl.MatrixTable:
+    '''
+    Annotate with RF bin, consequence, gene name, hgnc id, and pass/fail for 3 combinations of filters for SNPs,
+    3 combinations of filters for indels and missingness for each set of filters
+    :param hl.MatrixTable mtfile: Input matrixtable
+    :param str rf_htfile: random forest hail table file
+    :param str cqfile: consequence file
+    :return hl.matrixTable:
+    '''
+    rf_ht = hl.read_table(rf_htfile)
+    # keep only vars with rank_id = rank
+    rf_ht = rf_ht.filter(rf_ht.rank_id == 'rank')
+    # annotate mt with score and bin
+    mt = mt.annotate_rows(
+        info=mt.info.annotate(
+            rf_score=rf_ht[mt.row_key].score)
+    )
+    mt = mt.annotate_rows(
+        info=mt.info.annotate(
+            rf_bin=rf_ht[mt.row_key].bin)
+    )
+
+    cq_ht = hl.import_table(cqfile, types={'f0': 'str', 'f1': 'int32', 'f2': 'str',
+                            'f3': 'str', 'f4': 'str', 'f5': 'str', 'f6': 'str', 'f7': 'str', 'f8': 'str'}, no_header=True)
+    cq_ht = cq_ht.annotate(chr=cq_ht.f0)
+    cq_ht = cq_ht.annotate(pos=cq_ht.f1)
+    cq_ht = cq_ht.annotate(rs=cq_ht.f2)
+    cq_ht = cq_ht.annotate(ref=cq_ht.f3)
+    cq_ht = cq_ht.annotate(alt=cq_ht.f4)
+    cq_ht = cq_ht.annotate(CSQ=cq_ht.f5)
+    cq_ht = cq_ht.annotate(consequence=cq_ht.f6)
+    cq_ht = cq_ht.annotate(gene=cq_ht.f7)
+    cq_ht = cq_ht.annotate(hgnc_id=cq_ht.f8)
+    cq_ht = cq_ht.key_by(
+        locus=hl.locus(cq_ht.chr, cq_ht.pos), alleles=[cq_ht.ref, cq_ht.alt])
+    cq_ht = cq_ht.drop(cq_ht.f0, cq_ht.f1, cq_ht.f2, cq_ht.f3, cq_ht.f4, cq_ht.chr, cq_ht.pos, cq_ht.ref, cq_ht.alt)
+    cq_ht = cq_ht.key_by(cq_ht.locus, cq_ht.alleles)
+    mt = mt.annotate_rows(
+        info=mt.info.annotate(
+            CSQ=cq_ht[mt.row_key].CSQ,
+            consequence=cq_ht[mt.row_key].consequence,
+            gene=cq_ht[mt.row_key].gene,
+            hgnc_id=cq_ht[mt.row_key].hgnc_id)
+    )
+
+    return mt
+
+
+def apply_hard_filters(mt: hl.MatrixTable, hard_filters: dict) -> hl.MatrixTable:
+    '''
+    Apply hard filters and annotate missingness
+    :param hl.MatrixTable mt: Input MatrixTable
+    :param dict hard_filters: Filters to apply
+    :return hl.MatrixTable:
+    '''
+
+    stringent_condition = ( (
+                                (hl.is_snp(mt.alleles[0], mt.alleles[1])) 
+                                & (mt.info.rf_bin <= hard_filters['snp']['stringent']['bin'])
+                                & (mt.DP < hard_filters['snp']['stringent']['dp']) 
+                                & (mt.GQ < hard_filters['snp']['stringent']['gq']) 
+                                & ( (mt.GT.is_het() & (mt.HetAB < hard_filters['snp']['stringent']['ab'])) )
+                                ) |
+                                (
+                                (hl.is_indel(mt.alleles[0], mt.alleles[1])) 
+                                & (mt.info.rf_bin <= hard_filters['indel']['stringent']['bin'])
+                                & (mt.DP < hard_filters['indel']['stringent']['dp']) 
+                                & (mt.GQ < hard_filters['indel']['stringent']['gq']) 
+                                & ( (mt.GT.is_het() & (mt.HetAB < hard_filters['indel']['stringent']['ab'])) )
+                                )   
+                            )
+
+
+    medium_condition = ( ((hl.is_snp(mt.alleles[0], mt.alleles[1])) 
+                                & (mt.info.rf_bin <= hard_filters['snp']['medium']['bin'])
+                                & (mt.DP < hard_filters['snp']['medium']['dp']) 
+                                & (mt.GQ < hard_filters['snp']['medium']['gq']) 
+                                & ( (mt.GT.is_het() & (mt.HetAB < hard_filters['snp']['medium']['ab'])) ) 
+                                ) | 
+                                (
+                                (hl.is_indel(mt.alleles[0], mt.alleles[1])) 
+                                & (mt.info.rf_bin <= hard_filters['indel']['medium']['bin'])
+                                & (mt.DP < hard_filters['indel']['medium']['dp']) 
+                                & (mt.GQ < hard_filters['indel']['medium']['gq']) 
+                                & ( (mt.GT.is_het() & (mt.HetAB < hard_filters['indel']['medium']['ab'])) )
+                                )
+                            )
+    
+    relaxed_condition = ( ((hl.is_snp(mt.alleles[0], mt.alleles[1])) 
+                                & (mt.info.rf_bin <= hard_filters['snp']['relaxed']['bin'])
+                                & (mt.DP < hard_filters['snp']['relaxed']['dp']) 
+                                & (mt.GQ < hard_filters['snp']['relaxed']['gq']) 
+                                & ( (mt.GT.is_het() & (mt.HetAB < hard_filters['snp']['relaxed']['ab'])) )
+                                ) |
+                                (
+                                (hl.is_indel(mt.alleles[0], mt.alleles[1])) 
+                                & (mt.info.rf_bin <= hard_filters['indel']['relaxed']['bin'])
+                                & (mt.DP < hard_filters['indel']['relaxed']['dp']) 
+                                & (mt.GQ < hard_filters['indel']['relaxed']['gq']) 
+                                & ( (mt.GT.is_het() & (mt.HetAB < hard_filters['indel']['relaxed']['ab'])) ) 
+                                )                               
+                            )
+
+    mt = mt.annotate_entries(
+        stringent_filters = hl.if_else(stringent_condition, 'Fail', 'Pass'),
+        medium_filters = hl.if_else(medium_condition, 'Fail', 'Pass'),
+        relaxed_filters = hl.if_else(relaxed_condition, 'Fail', 'Pass')
+    )
+    #annotate variants with fraction passing/failing each set of filters
+    mt = mt.annotate_rows(stringent_pass_count = hl.agg.count_where(mt.stringent_filters == 'Pass'))
+    mt = mt.annotate_rows(stringent_fail_count = hl.agg.count_where(mt.stringent_filters == 'Fail'))
+    mt = mt.annotate_rows(info=mt.info.annotate(fraction_pass_stringent_filters = mt.stringent_pass_count/(mt.stringent_pass_count + mt.stringent_fail_count)))
+    
+    mt = mt.annotate_rows(medium_pass_count = hl.agg.count_where(mt.medium_filters == 'Pass'))
+    mt = mt.annotate_rows(medium_fail_count = hl.agg.count_where(mt.medium_filters == 'Fail'))
+    mt = mt.annotate_rows(info=mt.info.annotate(fraction_pass_medium_filters = mt.medium_pass_count/(mt.medium_pass_count + mt.medium_fail_count)))
+    
+    mt = mt.annotate_rows(relaxed_pass_count = hl.agg.count_where(mt.relaxed_filters == 'Pass'))
+    mt = mt.annotate_rows(relaxed_fail_count = hl.agg.count_where(mt.relaxed_filters == 'Fail'))
+    mt = mt.annotate_rows(info=mt.info.annotate(fraction_pass_relaxed_filters = mt.relaxed_pass_count/(mt.relaxed_pass_count + mt.relaxed_fail_count)))
+
+    return mt
+
+
+def main():
+    #set up
+    args = get_options()
+    inputs = parse_config()
+    mtdir = inputs['matrixtables_lustre_dir']
+    rf_dir = inputs['var_qc_rf_dir']
+    resourcedir = inputs['resource_dir']
+    hard_filters = inputs['hard_filters']
+
+    # initialise hail
+    tmp_dir = "hdfs://spark-master:9820/"
+    sc = pyspark.SparkContext()
+    hadoop_config = sc._jsc.hadoopConfiguration()
+    hl.init(sc=sc, tmp_dir=tmp_dir, default_reference="GRCh38")
+
+    rf_htfile = rf_dir + args.runhash + "/_gnomad_score_binning_tmp.ht"
+    mtfile = mtdir + "mt_varqc_splitmulti.mt"
+    cqfile = resourcedir + "all_consequences_with_gene_and_csq.txt"
+    mtfile_annot = mtdir + "mt_hard_filter_combinations.mt"
+
+    mt = hl.read_matrix_table(mtfile)
+
+    #annotate mt with consequence, gene, rf bin
+    mt_annot = annotate_cq_rf(mt, rf_htfile, cqfile)
+
+    #annotate with all combinations of filters (pass/fail) and add missingness
+    mt_annot = apply_hard_filters(mt_annot, hard_filters)
+    mt_annot.write(mtfile_annot, overwrite = True)
+
+
+if __name__ == '__main__':
+    main()
---
+++ 4-genotype_qc/3a-export_vcfs_range_of_hard_filters.py
@@ -0,0 +1,141 @@
+# export to VCF after annotating with a range of hard filters
+import hail as hl
+import pyspark
+import argparse
+from wes_qc.utils.utils import parse_config
+
+
+def get_options():
+    '''
+    Get options from the command line
+    '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--runhash", help="RF run hash")
+    args = parser.parse_args()
+    if not args.runhash:
+        print("--runhash must be specified")
+        exit(1)
+
+    return args
+
+
+def export_vcfs(mtfile: str, filtered_vcf_dir: str, hard_filters: dict, run_hash: str):
+    '''
+    Export VCFs annotated with a range of hard filters
+    :param str mtfile: matrixtable file
+    :param str filtered_vcf_dir: output directory for VCFs
+    :param dict hard_filters: details of all sets of filters
+    :param str run_hash: random forest run hash used
+    '''
+    mt = hl.read_matrix_table(mtfile)
+    # filter to remove rows where all variants fail the most relaxed filters
+    mt = mt.filter_rows(mt.info.fraction_pass_relaxed_filters > 0)
+    # drop unwanted fields
+    mt = mt.drop(mt.a_index, mt.was_split, mt.stringent_pass_count, mt.stringent_fail_count,
+                 mt.medium_pass_count, mt.medium_fail_count, mt.relaxed_pass_count, mt.relaxed_fail_count)
+    # info for header
+    stringent_filters = "SNPs: RF bin <= " + \
+        str(hard_filters['snp']['stringent']['bin']) + " & (DP < " + \
+        str(hard_filters['snp']['stringent']['dp']) + " | GQ < " + \
+        str(hard_filters['snp']['stringent']['gq']) + " | HetAB < " + \
+        str(hard_filters['snp']['stringent']['ab']) + "), Indes: RF bin <= " + \
+        str(hard_filters['indel']['stringent']['bin']) + " & (DP < " + \
+        str(hard_filters['indel']['stringent']['dp']) + " | GQ < " + \
+        str(hard_filters['indel']['stringent']['gq']) + " | HetAB < " + \
+        str(hard_filters['indel']['stringent']['ab']) + ")"
+
+    medium_filters = "SNPs: RF bin <= " + \
+        str(hard_filters['snp']['medium']['bin']) + " & (DP < " + \
+        str(hard_filters['snp']['medium']['dp']) + " | GQ < " + \
+        str(hard_filters['snp']['medium']['gq']) + " | HetAB < " + \
+        str(hard_filters['snp']['medium']['ab']) + "), Indes: RF bin <= " + \
+        str(hard_filters['indel']['medium']['bin']) + " & (DP < " + \
+        str(hard_filters['indel']['medium']['dp']) + " | GQ < " + \
+        str(hard_filters['indel']['medium']['gq']) + " | HetAB < " + \
+        str(hard_filters['indel']['medium']['ab']) + ")"
+
+    relaxed_filters = "SNPs: RF bin <= " + \
+        str(hard_filters['snp']['relaxed']['bin']) + " & (DP < " + \
+        str(hard_filters['snp']['relaxed']['dp']) + " | GQ < " + \
+        str(hard_filters['snp']['relaxed']['gq']) + " | HetAB < " + \
+        str(hard_filters['snp']['relaxed']['ab']) + "), Indes: RF bin <= " + \
+        str(hard_filters['indel']['relaxed']['bin']) + " & (DP < " + \
+        str(hard_filters['indel']['relaxed']['dp']) + " | GQ < " + \
+        str(hard_filters['indel']['relaxed']['gq']) + " | HetAB < " + \
+        str(hard_filters['indel']['relaxed']['ab']) + ")"
+
+    metadata = {
+        'format': {'HetAB': {'Description': 'Hetrozygous allele balance',
+                             'Number': 'A',
+                             'Type': 'Float'},
+                   'stringent_filters': {'Description': 'Pass/fail stringent hard filters ' + stringent_filters,
+                                         'Number': 'A',
+                                         'Type': 'String'},
+                   'medium_filters': {'Description': 'Pass/fail hard medium filters ' + medium_filters,
+                                      'Number': 'A',
+                                      'Type': 'String'},
+                   'relaxed_filters': {'Description': 'Pass/fail relaxed hard filters ' + relaxed_filters,
+                                       'Number': 'A',
+                                       'Type': 'String'},
+                   'CSQ': {'Description': 'Consequence annotations from Ensembl VEP. Format: Allele|Consequence|IMPACT|SYMBOL|Gene|Feature_type|Feature|BIOTYPE|EXON|INTRON|HGVSc|HGVSp|cDNA_position|CDS_position|Protein_position|Amino_acids|Codons|Existing_variation|ALLELE_NUM|DISTANCE|STRAND|FLAGS|VARIANT_CLASS|SYMBOL_SOURCE|HGNC_ID|CANONICAL|MANE_SELECT|MANE_PLUS_CLINICAL|TSL|APPRIS|CCDS|ENSP|SWISSPROT|TREMBL|UNIPARC|UNIPROT_ISOFORM|GENE_PHENO|SIFT|PolyPhen|DOMAINS|miRNA|HGVS_OFFSET|AF|AFR_AF|AMR_AF|EAS_AF|EUR_AF|SAS_AF|AA_AF|EA_AF|gnomAD_AF|gnomAD_AFR_AF|gnomAD_AMR_AF|gnomAD_ASJ_AF|gnomAD_EAS_AF|gnomAD_FIN_AF|gnomAD_NFE_AF|gnomAD_OTH_AF|gnomAD_SAS_AF|MAX_AF|MAX_AF_POPS|CLIN_SIG|SOMATIC|PHENO|PUBMED|MOTIF_NAME|MOTIF_POS|HIGH_INF_POS|MOTIF_SCORE_CHANGE|TRANSCRIPTION_FACTORS|SpliceRegion|GeneSplicer|existing_InFrame_oORFs|existing_OutOfFrame_oORFs|existing_uORFs|five_prime_UTR_variant_annotation|five_prime_UTR_variant_consequence|CADD_PHRED|CADD_RAW|Ensembl_transcriptid|LRT_pred|MutationTaster_pred|Polyphen2_HDIV_pred|Polyphen2_HVAR_pred|SIFT_pred|Uniprot_acc|VEP_canonical|DisGeNET_PMID|DisGeNET_SCORE|PHENOTYPES|Conservation|LoF|LoF_filter|LoF_flags|LoF_info|REVEL|SpliceAI_pred_DP_AG|SpliceAI_pred_DP_AL|SpliceAI_pred_DP_DG|SpliceAI_pred_DP_DL|SpliceAI_pred_DS_AG|SpliceAI_pred_DS_AL|SpliceAI_pred_DS_DG|SpliceAI_pred_DS_DL|SpliceAI_pred_SYMBOL',
+                           'Number': 'A',
+                           'Type': 'String'},
+                   'consequence': {'Description': 'Most severe consequence from VEP104',
+                                   'Number': 'A',
+                                   'Type': 'String'},
+                   'gene': {'Description': 'Gene affected by the most severe consequence from VEP104',
+                            'Number': 'A',
+                            'Type': 'String'},
+                   'hgnc_id': {'Description': 'HGNC id of the gene affected by the most severe consequence from VEP104',
+                               'Number': 'A',
+                               'Type': 'String'}
+                   },
+        'info': {
+            'fraction_pass_stringent_filters': {'Description': 'Fraction of genotypes which pass stringent hard filters ' + stringent_filters,
+                                                'Number': 'A',
+                                                'Type': 'Float'},
+            'fraction_pass_medium_filters': {'Description': 'Fraction of genotypes which pass medium hard filters ' + medium_filters,
+                                             'Number': 'A',
+                                             'Type': 'Float'},
+            'fraction_pass_relaxed_filters': {'Description': 'Fraction of genotypes which pass relaxed hard filters ' + relaxed_filters,
+                                              'Number': 'A',
+                                              'Type': 'Float'},
+            'rf_score': {'Description': 'Variant QC random forest score, model id ' + run_hash,
+                         'Number': 'A',
+                         'Type': 'Float'},
+            'rf_bin': {'Description': 'Variant QC random forest bin, model id ' + run_hash,
+                       'Number': 'A',
+                       'Type': 'Integer'}
+        }
+    }
+
+    #export per chromosome
+    chroms=[*range(1,23),"X","Y"]
+    chromosomes=["chr"+ str(chr) for chr in chroms]
+    for chromosome in chromosomes:
+        print("Exporting " + chromosome)
+        mt_chrom=mt.filter_rows(mt.locus.contig==chromosome)
+        outfile = filtered_vcf_dir + chromosome + "_hard_filters.vcf.bgz"
+        hl.export_vcf(mt_chrom, outfile, metadata = metadata)
+
+
+def main():
+    # set up
+    inputs = parse_config()
+    args = get_options()
+    mtdir = inputs['matrixtables_lustre_dir']
+    hard_filters = inputs['hard_filters']
+    filtered_vcf_dir = inputs['vcf_output_dir']
+
+    # initialise hail
+    tmp_dir = "hdfs://spark-master:9820/"
+    sc = pyspark.SparkContext()
+    hadoop_config = sc._jsc.hadoopConfiguration()
+    hl.init(sc=sc, tmp_dir=tmp_dir, default_reference="GRCh38")
+
+    mtfile = mtdir + "mt_hard_filter_combinations.mt"
+    export_vcfs(mtfile, filtered_vcf_dir, hard_filters, args.runhash)
+
+
+if __name__ == '__main__':
+    main()
