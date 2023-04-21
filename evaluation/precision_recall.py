64447dda5a8931ffa04c3d5a98bcb147ed49fbfa
Ruth <re3@sanger.ac.uk>
2023-04-21 14:17:55 +0100

    new thresholds

--- evaluation/precision_recall.py
+++ evaluation/precision_recall.py
@@ -21,7 +21,7 @@ def prepare_alspac_htfile(mtfile: str, rf_htfile: str, mtdir: str) -> hl.Table:
     filter_condition = (
         (mt.GT.is_het() & (mt.HetAB < 0.2)) | 
         (mt.DP < 5) |
-        (mt.GQ < 10)
+        (mt.GQ < 20)
     )
     mt = mt.annotate_entries(
         hard_filters = hl.if_else(filter_condition, 'Fail', 'Pass')
@@ -150,7 +150,7 @@ def print_results(results: dict, plot_dir: str):
     :param dict results: results dict
     :param str plotdir: Output plot directory
     '''
-    outfile = plot_dir + "/precision_recall_dp5_gq10_ab02.txt"
+    outfile = plot_dir + "/precision_recall_dp5_gq20_ab02.txt"
     n_bins = 102
     header = ("\t").join(['bin', 'snv_precision', 'snv_recall', 'indel_precision', 'indel_recall', 'snv_tp', 'snv_fp',
         'snv_fn', 'indel_tp', 'indel_fp', 'indel_fn'])
