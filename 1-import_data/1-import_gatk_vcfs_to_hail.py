8f818cc426f9aa1d299c122b1f446c99e6b0bc15
Ruth Eberhardt <re3@sanger.ac.uk>
2023-10-25 13:26:10 +0100

    new branch for BiB

---
+++ 1-import_data/1-import_gatk_vcfs_to_hail.py
@@ -0,0 +1,42 @@
+# Load GATK VCFs into hail and save as matrixtable
+import hail as hl
+import pyspark
+import yaml
+import os
+import sys
+from wes_qc.utils.utils import parse_config
+
+
+def load_vcfs_to_mt(indir, outdir, tmp_dir, header):
+    '''
+    load VCFs and save as hail mt
+    '''
+    objects = hl.utils.hadoop_ls(indir)
+    vcfs = [vcf["path"] for vcf in objects if (vcf["path"].startswith("file") and vcf["path"].endswith("vcf.gz"))]
+    print("Loading VCFs")
+    #create and save MT
+    mt = hl.import_vcf(vcfs, array_elements_required=False, force_bgz=True, header_file = header)
+    print("Saving as hail mt")
+    mt_out_file = outdir + "gatk_unprocessed.mt"
+    mt.write(mt_out_file, overwrite=True)
+
+
+def main():
+    #set up input variables
+    inputs = parse_config()
+    vcf_header = inputs['gatk_vcf_header']
+    import_vcf_dir = inputs['gatk_import_lustre_dir']
+    mtdir = inputs['load_matrixtables_lustre_dir']
+
+    #initialise hail
+    tmp_dir = "hdfs://spark-master:9820/"
+    sc = pyspark.SparkContext()
+    hadoop_config = sc._jsc.hadoopConfiguration()
+    hl.init(sc=sc, tmp_dir=tmp_dir, default_reference="GRCh38")
+
+    #load VCFs
+    load_vcfs_to_mt(import_vcf_dir, mtdir, tmp_dir, vcf_header)
+
+
+if __name__ == '__main__':
+    main() 
