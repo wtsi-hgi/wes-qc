c1e852fb287786dedf3fd3ec7d3b1538eb75fdb3
Ruth <re3@sanger.ac.uk>
2023-12-14 15:35:38 +0000

    fix dir to export pandas df

--- 2-sample_qc/3-population_pca_prediction.py
+++ 2-sample_qc/3-population_pca_prediction.py
@@ -149,6 +149,8 @@ def predict_pops(pca_scores_file: str, pop_ht_file: str, pop_ht_tsv):
     pop_ht_df2 =pop_ht_df[['s', 'pop']]
     new_row = pd.Series({'s':'EGAN00004311029', 'pop':'OTH'})
     pop_ht_df2 = append_row(pop_ht_df2, new_row)
+    print(pop_ht_df2)
+    print(pop_ht_tsv)
 
     pop_ht_df2.to_csv(pop_ht_tsv, sep = '\t')
     
