5dca421a56dcfca77e8afad7dc6160b12f8d31ac
Ruth <re3@sanger.ac.uk>
2023-12-20 15:55:23 +0000

    update for BiB

--- 2-sample_qc/6-filter_fail_sample_qc.py
+++ 2-sample_qc/6-filter_fail_sample_qc.py
@@ -4,7 +4,6 @@ import hail as hl
 import pyspark
 from wes_qc.utils.utils import parse_config
 
-
 def filter_to_sanger_only(annotated_mt_file: str, sanger_mt_file: str):
     '''
     param str annotated_mt_file: File containing MatrixTable annotated with sequencing location
@@ -15,16 +14,14 @@ def filter_to_sanger_only(annotated_mt_file: str, sanger_mt_file: str):
     '''
     mt = hl.read_matrix_table(annotated_mt_file)
     mt = mt.filter_cols(mt.sequencing_location == 'Sanger')  # filter to Sanger only
-    mt.write(sanger_mt_file, overwrite = True)
+    mt.write(sanger_mt_file)
 
 
-def remove_sample_qc_fails(sanger_mt_file: str, qc_filter_ht_file: str, samples_failing_qc_file: str, verifybamid_file: str, gtcheck_fail_file: str, sample_qc_filtered_mt_file: str):
+def remove_sample_qc_fails(sanger_mt_file: str, qc_filter_ht_file: str, samples_failing_qc_file: str, sample_qc_filtered_mt_file: str):
     '''
     param str sanger_mt_file: Input MatrixTable file
     param str qc_filter_ht_file: File contaning sample QC output
     param str samples_failing_qc_file: Output file for list of samples failing QC
-    param str verifybamid_file: VerfiyBamID2 results for all samples
-    param str gtcheck_fail_file: Samples which fail on gtcheck
     param str sample_qc_filtered_mt_file: Output file for MatrixTable with sample QC fails removed
     '''
     sangermt = hl.read_matrix_table(sanger_mt_file)
@@ -45,15 +42,6 @@ def remove_sample_qc_fails(sanger_mt_file: str, qc_filter_ht_file: str, samples_
     # filter the sangermt to remove samples that have failed
     fails = fails.key_by('s')
     sangermt = sangermt.filter_cols(hl.is_defined(fails[sangermt.s]), keep=False)
-    # remove samples failing gtcheck
-    gtcheck_ht = hl.import_table(gtcheck_fail_file,types={'f0':'str'}, no_header=True)
-    gtcheck_ht = gtcheck_ht.key_by('f0')
-    sangermt = sangermt.filter_cols(hl.is_defined(gtcheck_ht[sangermt.s]), keep=False)
-    # remove samples failing FREEMIX check
-    verifybamid_ht = hl.import_table(verifybamid_file, impute=True)
-    verifybamid_ht = verifybamid_ht.filter(verifybamid_ht.FREEMIX > 0.05)
-    verifybamid_ht = verifybamid_ht.key_by('SEQ_ID')
-    sangermt = sangermt.filter_cols(hl.is_defined(verifybamid_ht[sangermt.s]), keep=False)
     # save the filtered mt to file
     sangermt.write(sample_qc_filtered_mt_file, overwrite=True)
 
@@ -70,15 +58,11 @@ def main():
     hadoop_config = sc._jsc.hadoopConfiguration()
     hl.init(sc=sc, tmp_dir=tmp_dir, default_reference="GRCh38")
 
-    qc_filter_ht_file = mtdir + "mt_pops_QC_filters_sequencing_location_and_superpop.ht"
+    qc_filter_ht_file = mtdir + "mt_pops_QC_filters.ht"
     annotated_mt_file = mtdir + "gatk_unprocessed_with_pop_and_runid.mt"  # annotated but unfiltered mt
-    sample_qc_filtered_mt_file = mtdir + "mt_pops_QC_filters_sequencing_location_and_superpop_sanger_only_after_sample_qc.mt"
-    sanger_mt_file = mtdir + "mt_pops_QC_filters_sequencing_location_and_superpop_sanger_only_after_sample_qc_sanger_only.mt"
-    filter_to_sanger_only(annotated_mt_file, sanger_mt_file)
+    sample_qc_filtered_mt_file = mtdir + "mt_pops_QC_filters_after_sample_qc.mt"
     samples_failing_qc_file = annotdir + "samples_failing_qc.tsv.bgz"
-    verifybamid_file = annotdir + "verify_bam_id_result_concat.selfSM"
-    gtcheck_fail_file = annotdir + "sanger_samples_excluded_after_gtcheck.txt"
-    remove_sample_qc_fails(sanger_mt_file, qc_filter_ht_file, samples_failing_qc_file, verifybamid_file, gtcheck_fail_file, sample_qc_filtered_mt_file)
+    remove_sample_qc_fails(annotated_mt_file, qc_filter_ht_file, samples_failing_qc_file, sample_qc_filtered_mt_file)
 
 
 if __name__ == '__main__':
