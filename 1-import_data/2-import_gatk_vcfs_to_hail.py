f1b9d1e4cf6257e179d015de9d5492e0674efa7a
Gennadii Zakharov <gz3@mib119295s.internal.sanger.ac.uk>
2024-06-20 09:59:55 +0100

    Import data for Fenland cohort

--- 1-import_data/2-import_gatk_vcfs_to_hail.py
+++ 1-import_data/2-import_gatk_vcfs_to_hail.py
@@ -1,42 +1,48 @@
 # Load GATK VCFs into hail and save as matrixtable
 import hail as hl
-import pyspark
-import yaml
 import os
-import sys
-from wes_qc.utils.utils import parse_config
+
+from utils.utils import parse_config
+from wes_qc import hail_utils
 
 
-def load_vcfs_to_mt(indir, outdir, tmp_dir, header):
-    '''
+def load_vcfs_to_mt(indir: str, outdir: str, header: str) -> None:
+    """
     load VCFs and save as hail mt
-    '''
+    """
     objects = hl.utils.hadoop_ls(indir)
+    print("Collecting VCFs")
     vcfs = [vcf["path"] for vcf in objects if (vcf["path"].startswith("file") and vcf["path"].endswith("vcf.gz"))]
     print("Loading VCFs")
-    #create and save MT
-    mt = hl.import_vcf(vcfs, array_elements_required=False, force_bgz=True, header_file = header)
+    # create and save MT
+    mt = hl.import_vcf(vcfs, array_elements_required=False, force_bgz=True, header_file=header)
     print("Saving as hail mt")
-    mt_out_file = outdir + "gatk_unprocessed.mt"
+    mt_out_file = os.path.join(outdir, "gatk_unprocessed.mt")
     mt.write(mt_out_file, overwrite=True)
 
 
-def main():
-    #set up input variables
+def main() -> None:
+    # set up input variables
     inputs = parse_config()
-    vcf_header = inputs['gatk_vcf_header']
-    import_vcf_dir = inputs['gatk_import_lustre_dir']
-    mtdir = inputs['load_matrixtables_lustre_dir']
 
-    #initialise hail
-    tmp_dir = "hdfs://spark-master:9820/"
-    sc = pyspark.SparkContext()
-    hadoop_config = sc._jsc.hadoopConfiguration()
-    hl.init(sc=sc, tmp_dir=tmp_dir, default_reference="GRCh38")
+    data_root: str = inputs["data_root"]
+    dataset_name: str = inputs["dataset_name"]
 
-    #load VCFs
-    load_vcfs_to_mt(import_vcf_dir, mtdir, tmp_dir, vcf_header)
+    import_vcf_dir = os.path.join(data_root, inputs["gatk_import_lustre_dir"])
+    mtdir: str = os.path.join(data_root, inputs["matrixtables_lustre_dir"])
+    annot_dir: str = os.path.join(data_root, inputs["annotation_lustre_dir"])
+
+    tmp_dir: str = inputs["tmp_dir"]
+
+    vcf_header = os.path.join(data_root, annot_dir, inputs["gatk_vcf_header"])
+
+    # initialise hail
+    sc = hail_utils.init_hl(tmp_dir)
+
+    # load VCFs
+    load_vcfs_to_mt("file://" + import_vcf_dir, "file://" + mtdir, "file://" + vcf_header)
+    hail_utils.stop_hl(sc)
 
 
-if __name__ == '__main__':
-    main() 
+if __name__ == "__main__":
+    main()
--- 2-sample_qc/1-hard_filters_sex_annotation.py
+++ 2-sample_qc/1-hard_filters_sex_annotation.py
@@ -2,53 +2,58 @@
 # input gatk_unprocessed.mt from step 1.1
 import hail as hl
 import pyspark
-from wes_qc.utils.utils import parse_config
+from utils.utils import parse_config
+from wes_qc import hail_utils
 
 
 def apply_hard_filters(mt: hl.MatrixTable, mtdir: str) -> hl.MatrixTable:
-    '''
+    """
     Applies hard filters and annotates samples in the filtered set with call rate
     :param MatrixTable mt: MT containing samples to be ascertained for sex
     :param str mtdir: directory output matrix tables are written to
     :return: MatrixTable with hard filtering annotation
     :rtype: MatrixTable
-    '''
+    """
     print("Applying hard filters")
     filtered_mt_file = mtdir + "mt_hard_filters_annotated.mt"
     # Keeping only variations that:
     mt = mt.filter_rows(
-        (hl.len(mt.alleles) == 2) & hl.is_snp(mt.alleles[0], mt.alleles[1]) &  # Are SNPs
-        (hl.agg.mean(mt.GT.n_alt_alleles()) / 2 > 0.001) &  # Have alternate allele in at least 0.1% of samples
-        (hl.agg.fraction(hl.is_defined(mt.GT)) > 0.99)  # Have defined genotype at least in 99% of samples
+        (hl.len(mt.alleles) == 2)
+        & hl.is_snp(mt.alleles[0], mt.alleles[1])  # Are SNPs
+        & (hl.agg.mean(mt.GT.n_alt_alleles()) / 2 > 0.001)  # Have alternate allele in at least 0.1% of samples
+        & (hl.agg.fraction(hl.is_defined(mt.GT)) > 0.99)  # Have defined genotype at least in 99% of samples
     )
     mt = mt.annotate_cols(callrate=hl.agg.fraction(hl.is_defined(mt.GT)))
     mt.write(filtered_mt_file, overwrite=True)
     return mt
 
 
-def impute_sex(mt: hl.MatrixTable, mtdir: str, annotdir: str, male_threshold: float = 0.8,
-               female_threshold: float = 0.5) -> hl.MatrixTable:
-    '''
+def impute_sex(
+    mt: hl.MatrixTable, mtdir: str, annotdir: str, male_threshold: float = 0.8, female_threshold: float = 0.5
+) -> hl.MatrixTable:
+    """
     Imputes sex, exports data, and annotates mt with this data
     :param MatrixTable mt: MT containing samples to be ascertained for sex
     :param str mtdir: directory output matrix tables are written to
     :param str annotdir: directory annotation files are written to
     :return: MatrixTable with imputed sex annotations stashed in column annotation 'sex_check'
     :rtype: MatrixTable
-    '''
-    print("Imputing sex with male_threshold = " + str(male_threshold) + " and female threshold = " + str(
-        female_threshold))
+    """
+    print(
+        "Imputing sex with male_threshold = " + str(male_threshold) + " and female threshold = " + str(female_threshold)
+    )
 
     # filter to X and select unphased diploid genotypes
-    mt1 = hl.filter_intervals(mt, [hl.parse_locus_interval('chrX')])
+    mt1 = hl.filter_intervals(mt, [hl.parse_locus_interval("chrX")])
     mtx_unphased = mt1.select_entries(GT=hl.unphased_diploid_gt_index_call(mt1.GT.n_alt_alleles()))
     # impute sex on the unphased diploid GTs
-    sex_ht = hl.impute_sex(mtx_unphased.GT, aaf_threshold=0.05, female_threshold=female_threshold,
-                           male_threshold=male_threshold)
+    sex_ht = hl.impute_sex(
+        mtx_unphased.GT, aaf_threshold=0.05, female_threshold=female_threshold, male_threshold=male_threshold
+    )
     # export
-    sex_ht.export(annotdir + '/sex_annotated.sex_check.txt.bgz')
+    sex_ht.export(annotdir + "/sex_annotated.sex_check.txt.bgz")
     # annotate input (all chroms) mt with imputed sex and write to file
-    sex_colnames = ['f_stat', 'is_female']
+    sex_colnames = ["f_stat", "is_female"]
     sex_ht = sex_ht.select(*sex_colnames)
     mt = mt.annotate_cols(**sex_ht[mt.col_key])
     sex_mt_file = mtdir + "mt_sex_annotated.mt"
@@ -58,8 +63,8 @@ def impute_sex(mt: hl.MatrixTable, mtdir: str, annotdir: str, male_threshold: fl
     return mt
 
 
-def identify_inconsistencies(mt: hl.MatrixTable, mtdir: str, annotdir: str, resourcedir: str):
-    '''
+def identify_inconsistencies(mt: hl.MatrixTable, mtdir: str, annotdir: str, resourcedir: str) -> None:
+    """
     Find samples where annotated sex conflicts with the sex in our metadata
     Find samples where sex is not annotated
     Find samples where f_stat is between 0.2 and 0.8
@@ -67,50 +72,45 @@ def identify_inconsistencies(mt: hl.MatrixTable, mtdir: str, annotdir: str, reso
     :param str mtdir: directory output matrix tables are written to
     :param str annotdir: directory annotation files are written to
     :param str resourcedir: directory annotation files are written to
-    '''
+    """
     print("Annotating samples with inconsistencies:")
     qc_ht = mt.cols()
     # convert is_female boolean to sex
-    sex_expr = (hl.case()
-                .when(qc_ht.is_female, "female")
-                .when(qc_ht.is_female == False, "male")
-                .default("undetermined"))
-    qc_ht = qc_ht.annotate(sex=sex_expr).key_by('s')
+    sex_expr = hl.case().when(qc_ht.is_female, "female").when(qc_ht.is_female == False, "male").default("undetermined")
+    qc_ht = qc_ht.annotate(sex=sex_expr).key_by("s")
 
     # annotate with manifest sex - keyed on ega to match identifiers in matrixtable
-    metadata_file = resourcedir + '/all_samples_with_proceed_and_seq_info_and_warehouse_info.txt'
-    metadata_ht = hl.import_table(metadata_file, delimiter="\t").key_by('ega')
+    metadata_file = resourcedir + "/all_samples_with_proceed_and_seq_info_and_warehouse_info.txt"
+    metadata_ht = hl.import_table(metadata_file, delimiter="\t").key_by("ega")
     # we only want those from the metadata file where sex is known
-    metadata_ht = metadata_ht.filter((metadata_ht.sex == 'Male') | (metadata_ht.sex == 'Female'))
+    metadata_ht = metadata_ht.filter((metadata_ht.sex == "Male") | (metadata_ht.sex == "Female"))
 
     # annotate the sex-predictions with the manifest sex annotation - need to use a join here
     ht_joined = qc_ht.annotate(manifest_sex=metadata_ht[qc_ht.s].sex)
 
     # identify samples where imputed sex and manifest sex conflict
     conflicting_sex_ht = ht_joined.filter(
-        ((ht_joined.sex == 'male') & (ht_joined.manifest_sex == 'Female')) |
-        ((ht_joined.sex == 'female') & (ht_joined.manifest_sex == 'Male'))
+        ((ht_joined.sex == "male") & (ht_joined.manifest_sex == "Female"))
+        | ((ht_joined.sex == "female") & (ht_joined.manifest_sex == "Male"))
     )
-    conflicting_sex_ht.export(annotdir + '/conflicting_sex.txt.bgz')
+    conflicting_sex_ht.export(annotdir + "/conflicting_sex.txt.bgz")
 
     # identify samples where f stat is between 0.2 and 0.8
     f_stat_ht = qc_ht.filter((qc_ht.f_stat > 0.2) & (qc_ht.f_stat < 0.8))
-    f_stat_ht.export(annotdir + '/sex_annotation_f_stat_outliers.txt.bgz')
+    f_stat_ht.export(annotdir + "/sex_annotation_f_stat_outliers.txt.bgz")
 
 
-def main():
+def main() -> None:
     # set up
     inputs = parse_config()
-    importmtdir = inputs['load_matrixtables_lustre_dir']
-    mtdir = inputs['matrixtables_lustre_dir']
-    annotdir = inputs['annotation_lustre_dir']
-    resourcedir = inputs['resource_dir']
+    importmtdir = inputs["load_matrixtables_lustre_dir"]
+    mtdir = inputs["matrixtables_lustre_dir"]
+    annotdir = inputs["annotation_lustre_dir"]
+    resourcedir = inputs["resource_dir"]
 
     # initialise hail
     tmp_dir = inputs["tmp_dir"]
-    sc = pyspark.SparkContext()
-    hadoop_config = sc._jsc.hadoopConfiguration()
-    hl.init(sc=sc, tmp_dir=tmp_dir, default_reference="GRCh38")
+    sc = hail_utils.init_hl(tmp_dir)
 
     mt_in_file = mtdir + "/gatk_unprocessed.mt"
     print("Reading input matrix")
@@ -125,6 +125,8 @@ def main():
     # annotate_ambiguous_sex(mt_sex, mtdir)
     identify_inconsistencies(mt_sex, mtdir, annotdir, resourcedir)
 
+    hail_utils.stop_hl(sc)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
