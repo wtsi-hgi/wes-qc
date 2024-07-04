bf974898ef6b7d98879c6246f06cd5f922c79565
Gennadii Zakharov <gz3@mib119295s.internal.sanger.ac.uk>
2024-07-04 14:00:03 +0100

    Modified and improved sex annotation code. Added code to extract self-reported sex from FAM

--- 1-import_data/3-fam-to-sex-annotation.py
+++ 1-import_data/3-fam-to-sex-annotation.py
@@ -34,6 +34,33 @@ def extract_sex_from_multiple_fam(fam_files: list[str], output_csv: str) -> None
                         csv_writer.writerow([sample_id, sex])
 
 
+def change_ids_in_csv(input_csv: str, map_csv: str, output_csv: str) -> None:
+    # Load the mapping file into a dictionary
+    id_map = {}
+    with open(map_csv, mode="r") as map_file:
+        map_reader = csv.DictReader(map_file)
+        for row in map_reader:
+            id_map[row["OldID"]] = row["NewID"]
+
+    # Open the input CSV file and the output CSV file
+    with open(input_csv, mode="r") as infile, open(output_csv, mode="w", newline="") as outfile:
+        reader = csv.DictReader(infile)
+        fieldnames = reader.fieldnames if reader.fieldnames is not None else []
+        writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+
+        # Write the header to the output CSV file
+        writer.writeheader()
+
+        # Process each row in the input CSV file
+        for row in reader:
+            old_id = row["SampleID"]
+            if old_id in id_map:
+                row["SampleID"] = id_map[old_id]
+            else:
+                print(f"!!! WARNING: No new ID found for {old_id} in the map file.")
+            writer.writerow(row)
+
+
 def main() -> None:
     inputs = parse_config()
 
@@ -42,10 +69,19 @@ def main() -> None:
     annot_dir: str = os.path.join(data_root, inputs["annotation_lustre_dir"])
 
     fam_files: list[str] = [os.path.join(annot_dir, fam_file) for fam_file in inputs["fam_pedigree"]]
-    output_csv: str = os.path.join(annot_dir, inputs["sex_and_ethnicity_annotation_file"])
+    print("=== Extracting sex information from pedigree files: ===")
     print("\n".join(fam_files))
 
-    extract_sex_from_multiple_fam(fam_files, output_csv)
+    self_reported_sex: str = os.path.join(annot_dir, inputs["self_reported_sex"])
+    extract_sex_from_multiple_fam(fam_files, self_reported_sex)
+
+    if inputs["fam_id_remap"] is not None:
+        temp_self_reported_sex = os.path.join(annot_dir, "temp_self_reported_sex.csv")
+        id_map = os.path.join(annot_dir, inputs["fam_id_remap"])
+
+        os.rename(self_reported_sex, temp_self_reported_sex)
+        change_ids_in_csv(temp_self_reported_sex, id_map, self_reported_sex)
+    print(f"Saving sex annotation to {self_reported_sex}")
 
 
 if __name__ == "__main__":
--- 2-sample_qc/1-hard_filters_sex_annotation.py
+++ 2-sample_qc/1-hard_filters_sex_annotation.py
@@ -1,12 +1,15 @@
 # apply gnomad's hard filters and impute sex
 # input gatk_unprocessed.mt from step 1.1
+import argparse
+import os
+
 import hail as hl
 import pyspark
 from utils.utils import parse_config
 from wes_qc import hail_utils
 
 
-def apply_hard_filters(mt: hl.MatrixTable, mtdir: str) -> hl.MatrixTable:
+def apply_hard_filters(mt: hl.MatrixTable, mtdir: str) -> None:
     """
     Applies hard filters and annotates samples in the filtered set with call rate
     :param MatrixTable mt: MT containing samples to be ascertained for sex
@@ -15,7 +18,7 @@ def apply_hard_filters(mt: hl.MatrixTable, mtdir: str) -> hl.MatrixTable:
     :rtype: MatrixTable
     """
     print("Applying hard filters")
-    filtered_mt_file = mtdir + "mt_hard_filters_annotated.mt"
+    filtered_mt_file = "file://" + os.path.join(mtdir + "mt_hard_filters_annotated.mt")
     # Keeping only variations that:
     mt = mt.filter_rows(
         (hl.len(mt.alleles) == 2)
@@ -25,12 +28,11 @@ def apply_hard_filters(mt: hl.MatrixTable, mtdir: str) -> hl.MatrixTable:
     )
     mt = mt.annotate_cols(callrate=hl.agg.fraction(hl.is_defined(mt.GT)))
     mt.write(filtered_mt_file, overwrite=True)
-    return mt
 
 
 def impute_sex(
     mt: hl.MatrixTable, mtdir: str, annotdir: str, male_threshold: float = 0.8, female_threshold: float = 0.5
-) -> hl.MatrixTable:
+) -> None:
     """
     Imputes sex, exports data, and annotates mt with this data
     :param MatrixTable mt: MT containing samples to be ascertained for sex
@@ -51,19 +53,19 @@ def impute_sex(
         mtx_unphased.GT, aaf_threshold=0.05, female_threshold=female_threshold, male_threshold=male_threshold
     )
     # export
-    sex_ht.export(annotdir + "/sex_annotated.sex_check.txt.bgz")
+    sex_ht.export("file://" + os.path.join(annotdir, "sex_annotated.sex_check.csv.gz"))
     # annotate input (all chroms) mt with imputed sex and write to file
     sex_colnames = ["f_stat", "is_female"]
     sex_ht = sex_ht.select(*sex_colnames)
     mt = mt.annotate_cols(**sex_ht[mt.col_key])
-    sex_mt_file = mtdir + "mt_sex_annotated.mt"
+    sex_mt_file = "file://" + os.path.join(mtdir, "mt_sex_annotated.mt")
     print("Writing to " + sex_mt_file)
     mt.write(sex_mt_file, overwrite=True)
 
-    return mt
 
-
-def identify_inconsistencies(mt: hl.MatrixTable, mtdir: str, annotdir: str, resourcedir: str) -> None:
+def identify_inconsistencies(
+    mt: hl.MatrixTable, self_reported_sex: str, conflicting_sex: str, f_stat_outliers: str
+) -> None:
     """
     Find samples where annotated sex conflicts with the sex in our metadata
     Find samples where sex is not annotated
@@ -80,50 +82,71 @@ def identify_inconsistencies(mt: hl.MatrixTable, mtdir: str, annotdir: str, reso
     qc_ht = qc_ht.annotate(sex=sex_expr).key_by("s")
 
     # annotate with manifest sex - keyed on ega to match identifiers in matrixtable
-    metadata_file = resourcedir + "/all_samples_with_proceed_and_seq_info_and_warehouse_info.txt"
-    metadata_ht = hl.import_table(metadata_file, delimiter="\t").key_by("ega")
+    metadata_ht = hl.import_table("file://" + self_reported_sex, delimiter=",").key_by("SampleID")
     # we only want those from the metadata file where sex is known
-    metadata_ht = metadata_ht.filter((metadata_ht.sex == "Male") | (metadata_ht.sex == "Female"))
+    metadata_ht = metadata_ht.filter((metadata_ht.Sex == "Male") | (metadata_ht.Sex == "Female"))
 
     # annotate the sex-predictions with the manifest sex annotation - need to use a join here
-    ht_joined = qc_ht.annotate(manifest_sex=metadata_ht[qc_ht.s].sex)
+    ht_joined = qc_ht.annotate(manifest_sex=metadata_ht[qc_ht.s].Sex)
 
     # identify samples where imputed sex and manifest sex conflict
     conflicting_sex_ht = ht_joined.filter(
         ((ht_joined.sex == "male") & (ht_joined.manifest_sex == "Female"))
         | ((ht_joined.sex == "female") & (ht_joined.manifest_sex == "Male"))
     )
-    conflicting_sex_ht.export(annotdir + "/conflicting_sex.txt.bgz")
+    conflicting_sex_ht.export("file://" + conflicting_sex)
 
     # identify samples where f stat is between 0.2 and 0.8
     f_stat_ht = qc_ht.filter((qc_ht.f_stat > 0.2) & (qc_ht.f_stat < 0.8))
-    f_stat_ht.export(annotdir + "/sex_annotation_f_stat_outliers.txt.bgz")
+    f_stat_ht.export("file://" + f_stat_outliers)
 
 
 def main() -> None:
     # set up
     inputs = parse_config()
-    importmtdir = inputs["load_matrixtables_lustre_dir"]
-    mtdir = inputs["matrixtables_lustre_dir"]
-    annotdir = inputs["annotation_lustre_dir"]
+
+    data_root: str = inputs["data_root"]
+    dataset_name: str = inputs["dataset_name"]
+
+    mtdir: str = os.path.join(data_root, inputs["matrixtables_lustre_dir"])
+    annot_dir: str = os.path.join(data_root, inputs["annotation_lustre_dir"])
     resourcedir = inputs["resource_dir"]
 
     # initialise hail
     tmp_dir = inputs["tmp_dir"]
     sc = hail_utils.init_hl(tmp_dir)
 
-    mt_in_file = mtdir + "/gatk_unprocessed.mt"
-    print("Reading input matrix")
-    mt_unfiiltered = hl.read_matrix_table(mt_in_file)
+    # === Determinind state to work ====
 
-    # apply hard fitlers
-    mt_filtered = apply_hard_filters(mt_unfiiltered, mtdir)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--hard-filter", help="Apply hard filters", action="store_true")
+    parser.add_argument("-i", "--impute-sex", help="Impute sex information", action="store_true")
+    parser.add_argument("-c", "--check-consistency", help="Annotate and filter merged mt", action="store_true")
+    parser.add_argument("-r", "--run", help="Run all steps", action="store_true")
+    args = parser.parse_args()
+
+    if args.hard_filter or args.run:
+        mt_in_file = "file://" + os.path.join(mtdir, "gatk_unprocessed.mt")
+        mt_unfiltered = hl.read_matrix_table(mt_in_file)
+        # apply hard fitlers
+        apply_hard_filters(mt_unfiltered, mtdir)
 
     # impute sex
-    mt_sex = impute_sex(mt_filtered, mtdir, annotdir, male_threshold=0.6)
+    if args.impute_sex or args.run:
+        filtered_mt_file = "file://" + os.path.join(mtdir + "mt_hard_filters_annotated.mt")
+        mt_filtered = hl.read_matrix_table(filtered_mt_file)
+        impute_sex(mt_filtered, mtdir, annot_dir, male_threshold=0.6)
 
-    # annotate_ambiguous_sex(mt_sex, mtdir)
-    identify_inconsistencies(mt_sex, mtdir, annotdir, resourcedir)
+    # Check for conflicting sex
+    if args.check_consistency or args.run:
+        self_reported_sex = os.path.join(annot_dir, inputs["self_reported_sex"])
+        conflicting_sex = os.path.join(annot_dir, "conflicting_sex.tsv.gz")
+        f_stat_outliers = os.path.join(annot_dir, "sex_f_stat_outliers.tsv.gz")
+
+        sex_mt_file = "file://" + os.path.join(mtdir, "mt_sex_annotated.mt")
+        mt_sex = hl.read_matrix_table(sex_mt_file)
+
+        identify_inconsistencies(mt_sex, self_reported_sex, conflicting_sex, f_stat_outliers)
 
     hail_utils.stop_hl(sc)
 
