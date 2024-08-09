50e45c1000310864bfc891324f9c04714ef89392
Gennadii Zakharov <gz3@mib119295s.internal.sanger.ac.uk>
2024-08-09 16:12:00 +0100

    Refactored step 3/4 - application of the RF model

--- 3-variant_qc/4-apply_rf.py
+++ 3-variant_qc/4-apply_rf.py
@@ -1,48 +1,40 @@
-#apply RF model for variant QC
+# apply RF model for variant QC
+import os
+
 import hail as hl
 import pyspark
 import argparse
-from wes_qc.utils.utils import parse_config, get_rf
-import wes_qc.utils.constants as constants
+from utils import utils
+import utils.constants as constants
 from gnomad.variant_qc.random_forest import apply_rf_model, load_model
 
-
-def get_options():
-    '''
-    Get options from the command line
-    '''
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--runhash", help="RF training run hash")
-    args = parser.parse_args()
-    if not args.runhash:
-        print("--runhash must be specified")
-        exit(1)
-
-    return args
+from wes_qc import hail_utils
 
 
-def main():
+def main() -> None:
     # set up
-    args = get_options()
-    inputs = parse_config()
-    rf_dir = inputs['var_qc_rf_dir']
+    inputs = utils.parse_config()
+
+    data_root: str = inputs["data_root"]
+    rf_dir = os.path.join(data_root, inputs["var_qc_rf_dir"])
+    run_hash = inputs["runhash"]
 
     # initialise hail
-    tmp_dir = "hdfs://spark-master:9820/"
-    sc = pyspark.SparkContext()
-    hadoop_config = sc._jsc.hadoopConfiguration()
-    hl.init(sc=sc, tmp_dir=tmp_dir, default_reference="GRCh38")
+    sc = hail_utils.init_hl(inputs["tmp_dir"])
 
-    run_hash = args.runhash
-    rf_model = load_model(get_rf(rf_dir, data="model", run_hash=run_hash))
-    ht = get_rf(rf_dir, data="training", run_hash=run_hash).ht()
+    rf_model = load_model(utils.get_rf_model("file://" + rf_dir, run_hash=run_hash))
+    ht = utils.get_rf_data("file://" + rf_dir, data="training", run_hash=run_hash).ht()
     features = hl.eval(ht.features)
     ht = apply_rf_model(ht, rf_model, features, label=constants.LABEL_COL)
     ht = ht.annotate_globals(rf_hash=run_hash)
-    ht = ht.checkpoint(get_rf(rf_dir, "rf_result", run_hash=run_hash).path, overwrite=True)
-    ht_summary = ht.group_by("tp", "fp", constants.TRAIN_COL, constants.LABEL_COL, constants.PREDICTION_COL).aggregate(n=hl.agg.count())
+    ht = ht.checkpoint(utils.get_rf_data("file://" + rf_dir, "rf_result", run_hash=run_hash).path, overwrite=True)
+    ht_summary = ht.group_by("tp", "fp", constants.TRAIN_COL, constants.LABEL_COL, constants.PREDICTION_COL).aggregate(
+        n=hl.agg.count()
+    )
     ht_summary.show(n=20)
-    
 
-if __name__ == '__main__':
+    hail_utils.stop_hl(sc)
+
+
+if __name__ == "__main__":
     main()
