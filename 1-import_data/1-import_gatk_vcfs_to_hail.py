cd4d274522938dc188c7d4ab5b44188d718f7ea1
Yaroslav Popov <ip13@sanger.ac.uk>
2024-08-13 13:29:56 +0100

    import data

--- 1-import_data/1-import_gatk_vcfs_to_hail.py
+++ 1-import_data/1-import_gatk_vcfs_to_hail.py
@@ -4,27 +4,28 @@ import pyspark
 import yaml
 import os
 import sys
-from wes_qc.utils.utils import parse_config
+from utils.utils import parse_config
 
 
-def load_vcfs_to_mt(indir, outdir, tmp_dir, header):
+def load_vcfs_to_mt(indir, outdir):
     '''
     load VCFs and save as hail mt
     '''
     objects = hl.utils.hadoop_ls(indir)
     vcfs = [vcf["path"] for vcf in objects if (vcf["path"].startswith("file") and vcf["path"].endswith("vcf.gz"))]
+    assert len(vcfs) == 1
+
     print("Loading VCFs")
-    #create and save MT
-    mt = hl.import_vcf(vcfs, array_elements_required=False, force_bgz=True, header_file = header)
+    mt = hl.import_vcf(vcfs[0], array_elements_required=False, force_bgz=True)
+
     print("Saving as hail mt")
-    mt_out_file = outdir + "gatk_unprocessed.mt"
+    mt_out_file = os.path.join(outdir, "gatk_unprocessed.mt")
     mt.write(mt_out_file, overwrite=True)
 
 
 def main():
     #set up input variables
     inputs = parse_config()
-    vcf_header = inputs['gatk_vcf_header']
     import_vcf_dir = inputs['gatk_import_lustre_dir']
     mtdir = inputs['matrixtables_lustre_dir']
 
@@ -35,8 +36,8 @@ def main():
     hl.init(sc=sc, tmp_dir=tmp_dir, default_reference="GRCh38")
 
     #load VCFs
-    load_vcfs_to_mt(import_vcf_dir, mtdir, tmp_dir, vcf_header)
+    load_vcfs_to_mt(import_vcf_dir, mtdir)
 
 
 if __name__ == '__main__':
-    main() 
+    main()
