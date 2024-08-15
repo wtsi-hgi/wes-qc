40da42903e0e55c2d66760d59e1161d219c30fc5
Gennadii Zakharov <gz3@mib119295s.internal.sanger.ac.uk>
2024-08-15 14:58:18 +0100

    Refactored script 3/8. Implemented the range for threshold

--- 3-variant_qc/8-select_thresholds.py
+++ 3-variant_qc/8-select_thresholds.py
@@ -1,74 +1,79 @@
-# select thresholds for random forest - for a range of thresholds for SNPs and indels 
+# select thresholds for random forest - for a range of thresholds for SNPs and indels
 # calculate how many TPs are retained and how many FPs are included
+import os
+import re
+from typing import Any
+
 import hail as hl
-import pyspark
 import argparse
-from wes_qc.utils.utils import parse_config
+from utils.utils import parse_config
+
+from wes_qc import hail_utils
 
 
-def get_options():
-    '''
+def get_options() -> Any:
+    """
     Get options from the command line
-    '''
+    """
     parser = argparse.ArgumentParser()
-    parser.add_argument("--runhash", help="RF run hash")
-    parser.add_argument("--snv", help = "Proposed SNV threshold")
-    parser.add_argument("--indel", help = "Proposed Indel threshold")
+    parser.add_argument("--snv", required=True, help="Proposed SNV threshold. A singe value or a range")
+    parser.add_argument("--indel", required=True, help="Proposed Indel threshold. A singe value or a range")
     args = parser.parse_args()
-    if not args.runhash and args.snv and args.indel:
-        print("--runhash, --snv and --indel must be specified")
-        exit(1)
-    args.snv = int(args.snv)
-    args.indel = int(args.indel)
-
     return args
 
 
-def analyse_thresholds(htfile: str, snv_threshold: int, indel_threshold: int):
-    '''
+def analyse_thresholds(htfile: str, snv_threshold: str, indel_threshold: str) -> None:
+    """
     For suggested thresholds calculate what proportion of TP variants are included and what propeortion of FPs
     :param str htfile: ht file annotated with rank, bin, tp and fp
     :param int snv_threshold: proposed threshold for SNVs
     :param int indel_threshold: proposed threshold for indels
-    '''
-    #for each threshold also look at 3 bins on either side (unless <1 or >101)
+    """
+    # for each threshold also look at 3 bins on either side (unless <1 or >101)
     snv_t = get_threshold_range(snv_threshold)
     indel_t = get_threshold_range(indel_threshold)
-    
-    ht = hl.read_table(htfile)
-    #keep only those with rank_id = rank
-    ht = ht.filter(ht.rank_id == 'rank')
-    #split by snv and indel
-    snv_ht = ht.filter(ht.allele_type == 'snv')
-    indel_ht = ht.filter(ht.allele_type == 'snv', keep = False)
 
-    #calculate % TP kept and % FP kept at each threshold
+    ht = hl.read_table(htfile)
+    # keep only those with rank_id = rank
+    ht = ht.filter(ht.rank_id == "rank")
+    # split by snv and indel
+    snv_ht = ht.filter(ht.allele_type == "snv")
+    indel_ht = ht.filter(ht.allele_type == "snv", keep=False)
+
+    # calculate % TP kept and % FP kept at each threshold
     snv_result = get_vars_kept(snv_ht, snv_t)
     indel_result = get_vars_kept(indel_ht, indel_t)
-    #print results
+    # print results
     print("SNV percentage of variants kept at each threshold:")
     print_results(snv_result)
     print("Indel percentage of variants kept at each threshold:")
     print_results(indel_result)
 
 
-def print_results(result: dict):
-    '''
+def print_results(result: dict[int, Any]) -> None:
+    """
     Print percentages of variants kept at each threshold
     :param dict result: Results to print
-    '''
+    """
     for t in result.keys():
-        print("bin " + str(t) + ": " + '{0:.3f}'.format(result[t]['tp_pc']) + " percent TPs remain, "
-         + '{0:.3f}'.format(result[t]['fp_pc']) + " percent FPs remain" )
+        print(
+            "bin "
+            + str(t)
+            + ": "
+            + "{0:.3f}".format(result[t]["tp_pc"])
+            + " percent TPs remain, "
+            + "{0:.3f}".format(result[t]["fp_pc"])
+            + " percent FPs remain"
+        )
 
 
-def get_vars_kept(ht: hl.Table, t_list: list):
-    '''
+def get_vars_kept(ht: hl.Table, t_list: list[int]) -> dict[int, Any]:
+    """
     find proportions fo TP and FPs kept in ht for each threshold in t_list
     :param hl.Table ht: input Hail Table
     :param list t_list: List of thresholds
-    '''
-    results = {}
+    """
+    results: dict[int, Any] = {}
     ht_TP = ht.filter(ht.tp == True)
     ht_FP = ht.filter(ht.fp == True)
     tp_total = ht_TP.count()
@@ -77,48 +82,52 @@ def get_vars_kept(ht: hl.Table, t_list: list):
         results[t] = {}
         ht_tp_pass = ht_TP.filter(ht_TP.bin <= t)
         tp_pass = ht_tp_pass.count()
-        results[t]['tp_pc'] = 100*(tp_pass/tp_total)
+        results[t]["tp_pc"] = 100 * (tp_pass / tp_total)
         ht_fp_pass = ht_FP.filter(ht_FP.bin <= t)
         fp_pass = ht_fp_pass.count()
-        results[t]['fp_pc'] = 100*(fp_pass/fp_total)
+        results[t]["fp_pc"] = 100 * (fp_pass / fp_total)
 
     return results
-    
 
-def get_threshold_range(threshold: int):
-    '''
-    Given a single threshold value convert to a range - for each threshold also look at 3 bins on either 
-    side and +/-5 (unless <1 or >101)
-    :param int threshold: single threshold value
-    '''
-    t_list = list(range(threshold-3, threshold+4))
-    min_t = t_list[0]-5
-    max_t = t_list[6]+5
-    t_list.insert(0,min_t)
-    t_list.append(max_t)
-    while t_list[len(t_list)-1] > 101:
-        t_list.pop()
-    while(t_list[0] < 1):
-        t_list.pop[0]
+
+def get_threshold_range(threshold_str: str) -> list[int]:
+    """
+    Given a single threshold value convert to a range +/- 8  (unless <1 or >101)
+    Given a range notation generates the range
+    :param threshold: single threshold value or a list
+    """
+
+    if re.match(r"^\d+$", threshold_str):
+        threshold = int(threshold_str)
+        min_t = threshold - 8
+        max_t = threshold + 8
+    elif match := re.match(r"^(\d+)-(\d+)$", threshold_str):
+        min_t = int(match.group(1))
+        max_t = int(match.group(2))
+
+    t_list = list(range(max(1, min_t), min(max_t + 1, 101)))
 
     return t_list
 
 
-def main():
+def main() -> None:
     # set up
     args = get_options()
     inputs = parse_config()
-    rf_dir = inputs['var_qc_rf_dir']
+
+    data_root: str = inputs["data_root"]
+    dataset_name: str = inputs["dataset_name"]
+    rf_dir = os.path.join(data_root, inputs["var_qc_rf_dir"])
+    runhash = inputs["runhash"]
 
     # initialise hail
-    tmp_dir = "hdfs://spark-master:9820/"
-    sc = pyspark.SparkContext()
-    hadoop_config = sc._jsc.hadoopConfiguration()
-    hl.init(sc=sc, tmp_dir=tmp_dir, default_reference="GRCh38")
+    sc = hail_utils.init_hl(inputs["tmp_dir"])
 
-    htfile = rf_dir + args.runhash + "/_gnomad_score_binning_tmp.ht"
-    analyse_thresholds(htfile, args.snv, args.indel)
+    htfile = os.path.join(rf_dir, runhash, "_gnomad_score_binning_tmp.ht")
+    analyse_thresholds("file://" + htfile, args.snv, args.indel)
+
+    hail_utils.stop_hl(sc)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
