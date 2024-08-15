1beef0f4b730de3186202f3d422426a5e2afe210
Yaroslav Popov <ip13@sanger.ac.uk>
2024-08-15 11:27:11 +0100

    revert stratified_sample_qc to be general
    do identical sample QC for all population (no difference between AFR and others)

--- 2-sample_qc/4-find_population_outliers.py
+++ 2-sample_qc/4-find_population_outliers.py
@@ -1,12 +1,12 @@
 # perform hail sample QC stratified by superpopulation and identify outliers
 import hail as hl
 import pyspark
-import os
+import os.path
 from utils.utils import parse_config
 from gnomad.sample_qc.filtering import compute_stratified_metrics_filter
 
 
-def annotate_mt(raw_mt_file: str, pop_ht_file: str, annotated_mt_file: str, pop_pandas_file: str):
+def annotate_mt(raw_mt_file: str, pop_ht_file: str, annotated_mt_file: str):
     '''
     Annotate mt with superpopulation and sequencing runid
     :param str raw_mt_file: raw mt file
@@ -16,8 +16,7 @@ def annotate_mt(raw_mt_file: str, pop_ht_file: str, annotated_mt_file: str, pop_
     :param str pop_pandas_file: tsv from pandas df - EGA and pop
     '''
     mt = hl.read_matrix_table(raw_mt_file)
-    #pop_ht = hl.read_table(pop_ht_file)
-    pop_ht = hl.import_table(pop_pandas_file, delimiter="\t").key_by('s')
+    pop_ht = hl.read_table(pop_ht_file)
     mt = mt.annotate_cols(assigned_pop=pop_ht[mt.s].pop)
     mt.write(annotated_mt_file, overwrite=True)
 
@@ -66,40 +65,21 @@ def stratified_sample_qc(annotated_mt_file: str, mt_qc_outfile: str, ht_qc_cols_
     qc_metrics = ['heterozygosity_rate', 'n_snp', 'r_ti_tv', 'n_transition', 'n_transversion',
                   'r_insertion_deletion', 'n_insertion', 'n_deletion', 'r_het_hom_var']
 
-    pop_ht_afr = pop_ht.filter(pop_ht.assigned_pop == 'AFR')
-    pop_ht_nonafr = pop_ht.filter(pop_ht.assigned_pop != 'AFR')
-
-    pop_filter_ht_afr = compute_stratified_metrics_filter(
-        pop_ht_afr,
+    pop_filter_ht = compute_stratified_metrics_filter(
+        pop_ht,
         qc_metrics={
-            metric: pop_ht_afr.sample_qc[metric] for metric in qc_metrics
+            metric: pop_ht.sample_qc[metric] for metric in qc_metrics
         },
-        metric_threshold={'heterozygosity_rate': (100, 4),
-                          'r_het_hom_var': (100, 5)},
-
-        strata={"qc_pop": pop_ht_afr.assigned_pop},
+        strata={"qc_pop": pop_ht.assigned_pop},
     )
 
-    pop_filter_ht_nonafr = compute_stratified_metrics_filter(
-        pop_ht_nonafr,
-        qc_metrics={
-            metric: pop_ht_nonafr.sample_qc[metric] for metric in qc_metrics
-        },
-        metric_threshold={'heterozygosity_rate': (100, 4),
-                          'r_het_hom_var': (100, 4)},
+    globals = hl.eval(pop_filter_ht.globals.qc_metrics_stats)
 
-        strata={"qc_pop": pop_ht_nonafr.assigned_pop},
-    )
-
-    pop_filter_ht = pop_filter_ht_afr.union(pop_filter_ht_nonafr)
-    globals1 = hl.eval(pop_filter_ht_nonafr.globals.qc_metrics_stats)
-    globals2 = hl.eval(pop_filter_ht_afr.globals.qc_metrics_stats)
-    globals1.update(globals2)
-
-    pop_ht = pop_ht.annotate_globals(qc_metrics_stats=globals1)
+    pop_ht = pop_ht.annotate_globals(qc_metrics_stats=globals)
     pop_ht = pop_ht.annotate(**pop_filter_ht[pop_ht.key]).persist()
     checkpoint = pop_ht.aggregate(hl.agg.count_where(
-        hl.len(pop_ht.qc_metrics_filters) == 0))
+        hl.len(pop_ht.qc_metrics_filters) == 0)
+    )
     print(f'{checkpoint} exome samples found passing pop filtering')
     pop_ht.write(qc_filter_file, overwrite=True)
 
@@ -128,8 +108,7 @@ def main():
     raw_mt_file = os.path.join(mtdir, "gatk_unprocessed.mt")
     pop_ht_file = os.path.join(mtdir, "pop_assignments.ht")
     annotated_mt_file = os.path.join(mtdir, "gatk_unprocessed_with_pop.mt")
-    pop_pandas_file = os.path.join(mtdir, "pop_assignments.tsv")
-    annotate_mt(raw_mt_file, pop_ht_file, annotated_mt_file, pop_pandas_file)
+    annotate_mt(raw_mt_file, pop_ht_file, annotated_mt_file)
 
     # run sample QC and stratify by population
     mt_qc_outfile = os.path.join(mtdir, "mt_pops_sampleqc.mt")
