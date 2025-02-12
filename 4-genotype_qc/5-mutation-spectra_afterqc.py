588702565c65df7f4cb328968a9604e1cd5d68a2
Gennadii Zakharov <gz3@mib119295s.internal.sanger.ac.uk>
2025-02-12 14:44:59 +0000

    HSH-250: Fixed a bug with NaN in the mut spectra. Fixed pandas warning. Added hover for outliers.

--- 4-genotype_qc/5-mutation-spectra_afterqc.py
+++ 4-genotype_qc/5-mutation-spectra_afterqc.py
@@ -15,16 +15,18 @@ def main():
 
     # = STEP OUTPUTS = #
     mut_spectra_path = config["step4"]["plot_mutation_spectra_afterqc"]["mut_spectra_path"]
+    mut_spectra_plot_path = config["step4"]["plot_mutation_spectra_afterqc"]["mut_spectra_plot_path"]
 
     # = STEP LOGIC = #
     hail_utils.init_hl(tmp_dir)
 
     mt = hl.read_matrix_table(path_spark(mtpath))
     mut_spectra = visualize.calculate_mutation_spectra(mt)
+    mut_spectra.to_csv(mut_spectra_path, sep="\t")
 
     # Save the plot
     p = visualize.plot_mutation_spectra(mut_spectra, **config["step4"]["plot_mutation_spectra_afterqc"])
-    bokeh.io.output_file(mut_spectra_path)
+    bokeh.io.output_file(mut_spectra_plot_path)
     bokeh.io.save(p)
 
 
