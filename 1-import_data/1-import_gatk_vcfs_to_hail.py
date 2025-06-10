fb93dc3913d7686205b56d6a3af03697c9837c64
Gennadii Zakharov <gz3@mib119295s.internal.sanger.ac.uk>
2025-06-10 13:34:43 +0100

    HSH-225: Added checking for duplicated variations. Updated config parameters.

--- 1-import_data/1-import_gatk_vcfs_to_hail.py
+++ 1-import_data/1-import_gatk_vcfs_to_hail.py
@@ -39,9 +39,32 @@ def load_vcfs_to_mt(config):
         print("info: Loading VCFs WITHOUT header")
         mt = hl.import_vcf(vcfs, array_elements_required=False, force_bgz=True)
 
+
+    print("=== Checking for duplications ===")
+    duplicated_variants = find_duplicated_variants(mt)
+    if duplicated_variants.count() > 0:
+        dups_path = "{outfile}.duplicated_variants.vcf"
+        print(f"=== WARNING !!! Found duplicated variants. Exporting to: {dups_path}")
+        hl.export_vcf(duplicated_variants, path_spark(dups_path))
+    else:
+        print("=== Check for duplicates variants: No duplicates has been found. Everything looks good...")
+
     mt_out_file = path_spark(outfile)
     print(f"Saving as hail mt to {mt_out_file}")
     mt.write(mt_out_file, overwrite=True)
+
+def find_duplicated_variants(mt: hl.MatrixTable) -> hl.Table:
+    # Group by chromosome, position, and reference allele
+    grouped = mt.rows()
+    grouped = grouped.group_by(variation=(grouped.locus, grouped.alleles[0])).aggregate(var_count=hl.agg.count())
+
+    # Filter for duplicates
+    duplicates = grouped.filter(grouped.var_count > 1)
+    duplicates = duplicates.annotate(dup_locus=duplicates.variation[0])
+    duplicates = duplicates.key_by("dup_locus")
+
+    mt_duplicated_records = mt.filter_rows(hl.is_defined(duplicates[mt.locus]))
+    return mt_duplicated_records.rows()
 
 
 def main():
